79bf727ff93f7f8c0d46cc56ee0e1e5aa84cb7c4
ling990 <ling990@qq.com>
2022-09-13 11:55:31 +0800

    modify testcases Signed-off-by: ling990 <liling96@huawei.com>
    Signed-off-by: ling990 <ling990@qq.com>

--- test/fuzztest/vcodecfile_fuzzer/vcodecfile_fuzzer.cpp
+++ test/fuzztest/vcodecfile_fuzzer/vcodecfile_fuzzer.cpp
@@ -38,44 +38,41 @@ VCodecFileFuzzer::~VCodecFileFuzzer()
 
 bool VCodecFileFuzzer::FuzzVideoFile(uint8_t *data, size_t size)
 {
-    while (true) {
-        std::shared_ptr<VDecSignal> vdecSignal = std::make_shared<VDecSignal>();
-        vdecCallback_ = std::make_shared<VDecCallbackTest>(vdecSignal);
-        CHECK_INSTANCE_AND_RETURN_RET(vdecCallback_, false);
-        videoDec_ = std::make_shared<VDecMock>(vdecSignal);
-        CHECK_INSTANCE_AND_RETURN_RET(videoDec_, false);
-        CHECK_BOOL_AND_RETURN_RET(videoDec_->CreateVideoDecMockByMime("video/avc"), false);
-        CHECK_STATE_AND_RETURN_RET(videoDec_->SetCallback(vdecCallback_), false);
+    std::shared_ptr<VDecSignal> vdecSignal = std::make_shared<VDecSignal>();
+    vdecCallback_ = std::make_shared<VDecCallbackTest>(vdecSignal);
+    CHECK_INSTANCE_AND_RETURN_RET(vdecCallback_, false);
+    videoDec_ = std::make_shared<VDecMock>(vdecSignal);
+    CHECK_INSTANCE_AND_RETURN_RET(videoDec_, false);
+    CHECK_BOOL_AND_RETURN_RET(videoDec_->CreateVideoDecMockByMime("video/avc"), false);
+    CHECK_STATE_AND_RETURN_RET(videoDec_->SetCallback(vdecCallback_), false);
 
-        std::shared_ptr<VEncSignal> vencSignal = std::make_shared<VEncSignal>();
-        vencCallback_ = std::make_shared<VEncCallbackTest>(vencSignal);
-        CHECK_INSTANCE_AND_RETURN_RET(vencCallback_, false);
-        videoEnc_ = std::make_shared<VEncMock>(vencSignal);
-        CHECK_INSTANCE_AND_RETURN_RET(videoEnc_, false);
-        CHECK_BOOL_AND_RETURN_RET(videoEnc_->CreateVideoEncMockByMime("video/avc"), false);
-        CHECK_STATE_AND_RETURN_RET(videoEnc_->SetCallback(vencCallback_), false);
+    std::shared_ptr<VEncSignal> vencSignal = std::make_shared<VEncSignal>();
+    vencCallback_ = std::make_shared<VEncCallbackTest>(vencSignal);
+    CHECK_INSTANCE_AND_RETURN_RET(vencCallback_, false);
+    videoEnc_ = std::make_shared<VEncMock>(vencSignal);
+    CHECK_INSTANCE_AND_RETURN_RET(videoEnc_, false);
+    CHECK_BOOL_AND_RETURN_RET(videoEnc_->CreateVideoEncMockByMime("video/avc"), false);
+    CHECK_STATE_AND_RETURN_RET(videoEnc_->SetCallback(vencCallback_), false);
 
-        videoEnc_->SetOutPath("/data/test/media/avc_configurefuzz_out.es");
-        std::shared_ptr<FormatMock> format = AVCodecMockFactory::CreateFormat();
-        CHECK_INSTANCE_AND_RETURN_RET(format, false);
-        (void)format->PutIntValue("width", DEFAULT_WIDTH);
-        (void)format->PutIntValue("height", DEFAULT_HEIGHT);
-        (void)format->PutIntValue("pixel_format", NV12);
-        (void)format->PutIntValue("frame_rate", DEFAULT_FRAME_RATE);
-        const string path = "/data/test/media/fuzztest.h264";
-        CHECK_STATE_AND_RETURN_RET(WriteDataToFile(path, data, size), false);
-        videoDec_->SetSource(path, ES_H264, ES_LENGTH_H264);
-        videoEnc_->Configure(format);
-        videoDec_->Configure(format);
-        std::shared_ptr<SurfaceMock> surface = videoEnc_->GetInputSurface();
-        videoDec_->SetOutputSurface(surface);
-        videoDec_->Prepare();
-        videoEnc_->Prepare();
-        videoDec_->Start();
-        videoEnc_->Start();
-        sleep(WAITTING_TIME);
-        break;
-    }
+    videoEnc_->SetOutPath("/data/test/media/avc_configurefuzz_out.es");
+    std::shared_ptr<FormatMock> format = AVCodecMockFactory::CreateFormat();
+    CHECK_INSTANCE_AND_RETURN_RET(format, false);
+    (void)format->PutIntValue("width", DEFAULT_WIDTH);
+    (void)format->PutIntValue("height", DEFAULT_HEIGHT);
+    (void)format->PutIntValue("pixel_format", NV12);
+    (void)format->PutIntValue("frame_rate", DEFAULT_FRAME_RATE);
+    const string path = "/data/test/media/fuzztest.h264";
+    CHECK_STATE_AND_RETURN_RET(WriteDataToFile(path, data, size), false);
+    videoDec_->SetSource(path, ES_H264, ES_LENGTH_H264);
+    videoEnc_->Configure(format);
+    videoDec_->Configure(format);
+    std::shared_ptr<SurfaceMock> surface = videoEnc_->GetInputSurface();
+    videoDec_->SetOutputSurface(surface);
+    videoDec_->Prepare();
+    videoEnc_->Prepare();
+    videoDec_->Start();
+    videoEnc_->Start();
+    sleep(WAITTING_TIME);
     if (videoDec_ != nullptr) {
         videoDec_->Reset();
         videoDec_->Release();
