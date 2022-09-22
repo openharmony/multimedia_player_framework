d792aaab18f311c049a82cff430d90a665dfbbd1
NOBUGGERS <ruanmeng@huawei.com>
2022-09-22 11:45:35 +0000

    tdd 接口覆盖
    Signed-off-by: NOBUGGERS <ruanmeng@huawei.com>

--- frameworks/native/player/test/unittest/include/player_mock.h
+++ frameworks/native/player/test/unittest/include/player_mock.h
@@ -52,7 +52,7 @@ protected:
 class PlayerCallbackTest : public PlayerCallback, public NoCopyable, public PlayerSignal {
 public:
     ~PlayerCallbackTest() {}
-    void OnError(PlayerErrorType errorType, int32_t errorCode) override {}
+    void OnError(PlayerErrorType errorType, int32_t errorCode) override;
     void OnInfo(PlayerOnInfoType type, int32_t extra, const Format &infoBody) override;
     void SeekNotify(int32_t extra, const Format &infoBody);
     void Notify(PlayerStates currentState);
