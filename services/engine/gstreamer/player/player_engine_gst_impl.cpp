9363785d32d6cf765bf265a208890b8daefa0009
xxb-wzy <xuxiaobo5@huawei.com>
2022-09-24 15:51:34 +0800

    Signed-off-by: xxb-wzy <xuxiaobo5@huawei.com> Change-Id: I79ddbb638c2c6cff454f3cd0c361e7b63d8d4bec

--- services/engine/gstreamer/player/player_engine_gst_impl.cpp
+++ services/engine/gstreamer/player/player_engine_gst_impl.cpp
@@ -21,7 +21,6 @@
 #include "directory_ex.h"
 #include "audio_system_manager.h"
 #include "player_sinkprovider.h"
-#include "glib_mem_protect.h"
 
 namespace {
     constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, LOG_DOMAIN, "PlayerEngineGstImpl"};
@@ -51,7 +50,6 @@ PlayerEngineGstImpl::~PlayerEngineGstImpl()
 {
     (void)Reset();
     MEDIA_LOGD("0x%{public}06" PRIXPTR " Instances destroy", FAKE_POINTER(this));
-    GlibMemProtect::GetInstance().EngineExit();
 }
 
 bool PlayerEngineGstImpl::IsFileUrl(const std::string &url) const
