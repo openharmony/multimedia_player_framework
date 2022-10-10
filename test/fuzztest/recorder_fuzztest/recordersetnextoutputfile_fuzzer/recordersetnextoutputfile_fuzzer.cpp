4a6db3b128784a06497225ce14c5351b9ba69f90
monissa <qiaofenfeng@huawei.com>
2022-10-10 10:11:59 +0800

    录制3个用例 RecorderSetVideoSourceFuzzTest/RecorderSetMaxFileSizeFuzzTest/RecorderSetNextOutputFileFuzzTest Signed-off-by: monissa<qiaofenfeng@huawei.com>
    Signed-off-by: monissa <qiaofenfeng@huawei.com>

---
+++ test/fuzztest/recorder_fuzztest/recordersetnextoutputfile_fuzzer/recordersetnextoutputfile_fuzzer.cpp
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "recordersetnextoutputfile_fuzzer.h"
+#include <iostream>
+#include "aw_common.h"
+#include "string_ex.h"
+#include "media_errors.h"
+#include "directory_ex.h"
+#include "recorder.h"
+
+using namespace std;
+using namespace OHOS;
+using namespace Media;
+using namespace PlayerTestParam;
+using namespace RecorderTestParam;
+
+namespace OHOS {
+namespace Media {
+RecorderSetNextOutputFileFuzzer::RecorderSetNextOutputFileFuzzer()
+{
+}
+
+RecorderSetNextOutputFileFuzzer::~RecorderSetNextOutputFileFuzzer()
+{
+}
+
+bool RecorderSetNextOutputFileFuzzer::FuzzRecorderSetNextOutputFile(uint8_t *data, size_t size)
+{
+    RETURN_IF(TestRecorder::CreateRecorder(), false);
+
+    static VideoRecorderConfig_ g_videoRecorderConfig;
+
+    g_videoRecorderConfig.vSource = VIDEO_SOURCE_SURFACE_YUV;
+    g_videoRecorderConfig.videoFormat = MPEG4;
+    g_videoRecorderConfig.outputFd = open("/data/test/media/recorder_SetNextOutputFile.mp4", O_RDWR);
+
+    if (g_videoRecorderConfig.outputFd > 0)
+    {
+        RETURN_IF(TestRecorder::SetVideoSource(g_videoRecorderConfig), false);
+        RETURN_IF(TestRecorder::SetOutputFormat(g_videoRecorderConfig), false);
+        RETURN_IF(TestRecorder::CameraServicesForVideo(g_videoRecorderConfig), false);
+        RETURN_IF(TestRecorder::SetCaptureRate(g_videoRecorderConfig, 30), false);
+        RETURN_IF(TestRecorder::SetMaxFileSize(5000, g_videoRecorderConfig), false);
+        
+        g_videoRecorderConfig.outputFd = *reinterpret_cast<int32_t *>(data);
+
+        RETURN_IF(TestRecorder::SetNextOutputFile(g_videoRecorderConfig), true);
+    }
+    close(g_videoRecorderConfig.outputFd);
+    return true;
+}
+}
+
+bool FuzzTestRecorderSetNextOutputFile(uint8_t *data, size_t size)
+{
+    if (data == nullptr) {
+        return true;
+    }
+
+    if (size < sizeof(int32_t)) {
+        return true;
+    }
+    RecorderSetNextOutputFileFuzzer testRecorder;
+    return testRecorder.FuzzRecorderSetNextOutputFile(data, size);
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::FuzzTestRecorderSetNextOutputFile(data, size);
+    return 0;
+}
---
+++ test/fuzztest/recorder_fuzztest/recordersetvideosource_fuzzer/recordersetvideosource_fuzzer.cpp
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "recordersetvideosource_fuzzer.h"
+#include <iostream>
+#include "aw_common.h"
+#include "string_ex.h"
+#include "media_errors.h"
+#include "directory_ex.h"
+#include "recorder.h"
+
+using namespace std;
+using namespace OHOS;
+using namespace Media;
+using namespace PlayerTestParam;
+using namespace RecorderTestParam;
+
+namespace OHOS {
+namespace Media {
+RecorderSetVideoSourceFuzzer::RecorderSetVideoSourceFuzzer()
+{
+}
+
+RecorderSetVideoSourceFuzzer::~RecorderSetVideoSourceFuzzer()
+{
+}
+
+bool RecorderSetVideoSourceFuzzer::FuzzRecorderSetVideoSource(uint8_t *data, size_t size)
+{
+    constexpr uint32_t recorderTime = 5;
+    constexpr int32_t videoSourceTypeList = 3;
+    RETURN_IF(TestRecorder::CreateRecorder(), false);
+
+    static VideoRecorderConfig_ g_videoRecorderConfig;
+    VideoSourceType VideoSourceTypes[videoSourceTypeList] {
+        VideoSourceType::VIDEO_SOURCE_SURFACE_YUV,
+        VideoSourceType::VIDEO_SOURCE_SURFACE_ES,
+        VideoSourceType::VIDEO_SOURCE_BUTT
+    };
+    int32_t sourcesubscript = *reinterpret_cast<int32_t *>(data) % (videoSourceTypeList);
+
+    g_videoRecorderConfig.vSource = VideoSourceTypes[sourcesubscript];
+    g_videoRecorderConfig.videoFormat = MPEG4;
+    g_videoRecorderConfig.outputFd = open("/data/test/media/recorder_SetVideoSource.mp4", O_RDWR);
+    
+    if (g_videoRecorderConfig.outputFd >= 0) {
+        cout << "1" << endl;
+        RETURN_IF(TestRecorder::SetVideoSource(g_videoRecorderConfig), true);
+        cout << "2" << endl;
+        RETURN_IF(TestRecorder::SetOutputFormat(g_videoRecorderConfig), true);
+        RETURN_IF(TestRecorder::CameraServicesForVideo(g_videoRecorderConfig), true);
+        RETURN_IF(TestRecorder::SetMaxDuration(g_videoRecorderConfig), true);
+        RETURN_IF(TestRecorder::SetOutputFile(g_videoRecorderConfig), true) ;
+        RETURN_IF(TestRecorder::SetRecorderCallback(g_videoRecorderConfig), true);
+        RETURN_IF(TestRecorder::Prepare(g_videoRecorderConfig), true);
+        RETURN_IF(TestRecorder::RequesetBuffer(PURE_VIDEO, g_videoRecorderConfig), true);
+        RETURN_IF(TestRecorder::Start(g_videoRecorderConfig), true);
+        sleep(recorderTime);
+        RETURN_IF(TestRecorder::Stop(false, g_videoRecorderConfig), true);
+        StopBuffer(PURE_VIDEO);
+        RETURN_IF(TestRecorder::Reset(g_videoRecorderConfig), true);
+        RETURN_IF(TestRecorder::Release(g_videoRecorderConfig), true);
+    }
+    close(g_videoRecorderConfig.outputFd);
+    return true;
+}
+}
+
+bool FuzzTestRecorderSetSource(uint8_t *data, size_t size)
+{
+    if (data == nullptr) {
+        return true;
+    }
+
+    if (size < sizeof(int32_t)) {
+        return true;
+    }
+    RecorderSetVideoSourceFuzzer testrecorder;
+    return testrecorder.FuzzRecorderSetVideoSource(data, size);
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::FuzzTestRecorderSetSource(data, size);
+    return 0;
+}
+
