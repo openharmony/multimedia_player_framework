42a2487b2cb4f128209c5bde343556c5c37eacc1
x30034819 <xieyijun3@huawei.com>
2022-11-01 17:30:21 +0800

    录制输入buffer size校验 Signed-off-by: x30034819 <xieyijun3@huawei.com>
    Signed-off-by: x30034819 <xieyijun3@huawei.com>
    Change-Id: I3ce2cd0133656f6c0ae26542430333e3ca4bed7c

--- services/engine/gstreamer/plugins/source/memsource/gst_consumer_surface_allocator.cpp
+++ services/engine/gstreamer/plugins/source/memsource/gst_consumer_surface_allocator.cpp
@@ -82,8 +82,10 @@ static GstMemory *gst_consumer_surface_allocator_alloc(GstAllocator *allocator, 
 
     pixel_format = surface_buffer->GetFormat();
 
+    gsize realsize = surface_buffer->GetSize() < size ? surface_buffer->GetSize() : size;
+
     gst_memory_init(GST_MEMORY_CAST(mem), GST_MEMORY_FLAG_NO_SHARE,
-        allocator, nullptr, surface_buffer->GetSize(), 0, 0, size);
+        allocator, nullptr, surface_buffer->GetSize(), 0, 0, realsize);
     mem->surface_buffer = surface_buffer;
     mem->fencefd = fencefd;
     mem->timestamp = timestamp;
