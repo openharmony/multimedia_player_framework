efd6b50cba1f58a9bce00f3640d3f0b1cd579f41
huang-xl <huangxiaolong1@huawei.com>
2022-11-04 18:24:02 +0800

    media_service deadlock Signed-off-by: huang-xl<huangxiaolong1@huawei.com>
    Signed-off-by: huang-xl <huangxiaolong1@huawei.com>
    Change-Id: I0ea5212c4cc2b566bdea8049ea092e76f3040e11

--- services/services/sa_media/ipc/media_service_stub.cpp
+++ services/services/sa_media/ipc/media_service_stub.cpp
@@ -139,9 +139,9 @@ int32_t MediaServiceStub::GetSystemAbility(MessageParcel &data, MessageParcel &r
 {
     MediaSystemAbility id = static_cast<MediaSystemAbility>(data.ReadInt32());
     sptr<IRemoteObject> listenerObj = data.ReadRemoteObject();
-    int32_t id = PlayerXCollie::GetInstance().SetTimer("MediaServiceStub::GetSystemAbility", true);
+    int32_t xcollieId = PlayerXCollie::GetInstance().SetTimer("MediaServiceStub::GetSystemAbility", true);
     (void)reply.WriteRemoteObject(GetSubSystemAbility(id, listenerObj));
-    PlayerXCollie::GetInstance().CancelTimer(id);
+    PlayerXCollie::GetInstance().CancelTimer(xcollieId);
     return MSERR_OK;
 }
 } // namespace Media
