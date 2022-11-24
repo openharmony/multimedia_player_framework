e5951dba5ec85594d9e7f21f835e61e46e01249a
NOBUGGERS <ruanmeng@huawei.com>
2022-11-24 11:07:47 +0000

    打印修改
    Signed-off-by: NOBUGGERS <ruanmeng@huawei.com>

--- frameworks/native/player/player_impl.cpp
+++ frameworks/native/player/player_impl.cpp
@@ -26,7 +26,7 @@ namespace OHOS {
 namespace Media {
 std::shared_ptr<Player> PlayerFactory::CreatePlayer()
 {
-    MEDIA_LOGD("PlayerImpl CreatePlayer in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl CreatePlayer in");
     std::shared_ptr<PlayerImpl> impl = std::make_shared<PlayerImpl>();
     CHECK_AND_RETURN_RET_LOG(impl != nullptr, nullptr, "failed to new PlayerImpl");
 
@@ -38,7 +38,7 @@ std::shared_ptr<Player> PlayerFactory::CreatePlayer()
 
 int32_t PlayerImpl::Init()
 {
-    MEDIA_LOGD("PlayerImpl Init in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl Init in");
     playerService_ = MediaServiceFactory::GetInstance().CreatePlayerService();
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_UNKNOWN, "failed to create player service");
     return MSERR_OK;
@@ -46,7 +46,7 @@ int32_t PlayerImpl::Init()
 
 PlayerImpl::PlayerImpl()
 {
-    MEDIA_LOGD("PlayerImpl:0x%{public}06" PRIXPTR " Instances create", FAKE_POINTER(this));
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl:0x%{public}06" PRIXPTR " Instances create", FAKE_POINTER(this));
 }
 
 PlayerImpl::~PlayerImpl()
@@ -55,19 +55,19 @@ PlayerImpl::~PlayerImpl()
         (void)MediaServiceFactory::GetInstance().DestroyPlayerService(playerService_);
         playerService_ = nullptr;
     }
-    MEDIA_LOGD("PlayerImpl:0x%{public}06" PRIXPTR " Instances destroy", FAKE_POINTER(this));
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl:0x%{public}06" PRIXPTR " Instances destroy", FAKE_POINTER(this));
 }
 
 int32_t PlayerImpl::SetSource(const std::shared_ptr<IMediaDataSource> &dataSrc)
 {
-    MEDIA_LOGD("PlayerImpl SetSource in(dataSrc)");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl SetSource in(dataSrc)");
     CHECK_AND_RETURN_RET_LOG(dataSrc != nullptr, MSERR_INVALID_VAL, "failed to create data source");
     return playerService_->SetSource(dataSrc);
 }
 
 int32_t PlayerImpl::SetSource(const std::string &url)
 {
-    MEDIA_LOGD("PlayerImpl SetSource in(url): %{public}s", url.c_str());
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl SetSource in(url): %{public}s", url.c_str());
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     CHECK_AND_RETURN_RET_LOG(!url.empty(), MSERR_INVALID_VAL, "url is empty..");
     return playerService_->SetSource(url);
@@ -75,56 +75,56 @@ int32_t PlayerImpl::SetSource(const std::string &url)
 
 int32_t PlayerImpl::SetSource(int32_t fd, int64_t offset, int64_t size)
 {
-    MEDIA_LOGD("PlayerImpl SetSource in(fd)");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl SetSource in(fd)");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->SetSource(fd, offset, size);
 }
 
 int32_t PlayerImpl::Play()
 {
-    MEDIA_LOGD("PlayerImpl Play in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl Play in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->Play();
 }
 
 int32_t PlayerImpl::Prepare()
 {
-    MEDIA_LOGD("PlayerImpl Prepare in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl Prepare in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->Prepare();
 }
 
 int32_t PlayerImpl::PrepareAsync()
 {
-    MEDIA_LOGD("PlayerImpl PrepareAsync in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl PrepareAsync in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->PrepareAsync();
 }
 
 int32_t PlayerImpl::Pause()
 {
-    MEDIA_LOGD("PlayerImpl Pause in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl Pause in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->Pause();
 }
 
 int32_t PlayerImpl::Stop()
 {
-    MEDIA_LOGD("PlayerImpl Stop in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl Stop in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->Stop();
 }
 
 int32_t PlayerImpl::Reset()
 {
-    MEDIA_LOGD("PlayerImpl Reset in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl Reset in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->Reset();
 }
 
 int32_t PlayerImpl::Release()
 {
-    MEDIA_LOGD("PlayerImpl Release in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl Release in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     (void)playerService_->Release();
     (void)MediaServiceFactory::GetInstance().DestroyPlayerService(playerService_);
@@ -134,77 +134,77 @@ int32_t PlayerImpl::Release()
 
 int32_t PlayerImpl::SetVolume(float leftVolume, float rightVolume)
 {
-    MEDIA_LOGD("PlayerImpl SetVolume(%{public}f, %{public}f) in", leftVolume, rightVolume);
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl SetVolume(%{public}f, %{public}f) in", leftVolume, rightVolume);
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->SetVolume(leftVolume, rightVolume);
 }
 
 int32_t PlayerImpl::Seek(int32_t mSeconds, PlayerSeekMode mode)
 {
-    MEDIA_LOGD("PlayerImpl Seek in, seek to %{public}d ms, mode is %{public}d", mSeconds, mode);
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl Seek in, seek to %{public}d ms, mode is %{public}d", mSeconds, mode);
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->Seek(mSeconds, mode);
 }
 
 int32_t PlayerImpl::GetCurrentTime(int32_t &currentTime)
 {
-    MEDIA_LOGD("PlayerImpl GetCurrentTime in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl GetCurrentTime in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->GetCurrentTime(currentTime);
 }
 
 int32_t PlayerImpl::GetVideoTrackInfo(std::vector<Format> &videoTrack)
 {
-    MEDIA_LOGD("PlayerImpl GetVideoTrackInfo in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl GetVideoTrackInfo in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->GetVideoTrackInfo(videoTrack);
 }
 
 int32_t PlayerImpl::GetAudioTrackInfo(std::vector<Format> &audioTrack)
 {
-    MEDIA_LOGD("PlayerImpl GetAudioTrackInfo in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl GetAudioTrackInfo in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->GetAudioTrackInfo(audioTrack);
 }
 
 int32_t PlayerImpl::GetVideoWidth()
 {
-    MEDIA_LOGD("PlayerImpl GetVideoWidth in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl GetVideoWidth in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->GetVideoWidth();
 }
 
 int32_t PlayerImpl::GetVideoHeight()
 {
-    MEDIA_LOGD("PlayerImpl GetVideoHeight in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl GetVideoHeight in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->GetVideoHeight();
 }
 
 int32_t PlayerImpl::SetPlaybackSpeed(PlaybackRateMode mode)
 {
-    MEDIA_LOGD("PlayerImpl SetPlaybackSpeed in, mode is %{public}d", mode);
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl SetPlaybackSpeed in, mode is %{public}d", mode);
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->SetPlaybackSpeed(mode);
 }
 
 int32_t PlayerImpl::GetPlaybackSpeed(PlaybackRateMode &mode)
 {
-    MEDIA_LOGD("PlayerImpl GetPlaybackSpeed in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl GetPlaybackSpeed in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->GetPlaybackSpeed(mode);
 }
 
 int32_t PlayerImpl::SelectBitRate(uint32_t bitRate)
 {
-    MEDIA_LOGD("PlayerImpl SelectBitRate(%{public}d) in", bitRate);
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl SelectBitRate(%{public}d) in", bitRate);
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->SelectBitRate(bitRate);
 }
 
 int32_t PlayerImpl::GetDuration(int32_t &duration)
 {
-    MEDIA_LOGD("PlayerImpl GetDuration in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl GetDuration in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->GetDuration(duration);
 }
@@ -212,7 +212,7 @@ int32_t PlayerImpl::GetDuration(int32_t &duration)
 #ifdef SUPPORT_VIDEO
 int32_t PlayerImpl::SetVideoSurface(sptr<Surface> surface)
 {
-    MEDIA_LOGD("PlayerImpl SetVideoSurface in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl SetVideoSurface in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     CHECK_AND_RETURN_RET_LOG(surface != nullptr, MSERR_INVALID_VAL, "surface is nullptr");
     surface_ = surface;
@@ -222,7 +222,7 @@ int32_t PlayerImpl::SetVideoSurface(sptr<Surface> surface)
 
 bool PlayerImpl::IsPlaying()
 {
-    MEDIA_LOGD("PlayerImpl IsPlaying in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl IsPlaying in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, false, "player service does not exist..");
 
     return playerService_->IsPlaying();
@@ -230,7 +230,7 @@ bool PlayerImpl::IsPlaying()
 
 bool PlayerImpl::IsLooping()
 {
-    MEDIA_LOGD("PlayerImpl IsLooping in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl IsLooping in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, false, "player service does not exist..");
 
     return playerService_->IsLooping();
@@ -238,14 +238,14 @@ bool PlayerImpl::IsLooping()
 
 int32_t PlayerImpl::SetLooping(bool loop)
 {
-    MEDIA_LOGD("PlayerImpl SetLooping in, loop %{public}d", loop);
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl SetLooping in, loop %{public}d", loop);
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->SetLooping(loop);
 }
 
 int32_t PlayerImpl::SetPlayerCallback(const std::shared_ptr<PlayerCallback> &callback)
 {
-    MEDIA_LOGD("PlayerImpl SetPlayerCallback in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl SetPlayerCallback in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     CHECK_AND_RETURN_RET_LOG(callback != nullptr, MSERR_INVALID_VAL, "callback is nullptr");
     return playerService_->SetPlayerCallback(callback);
@@ -253,7 +253,7 @@ int32_t PlayerImpl::SetPlayerCallback(const std::shared_ptr<PlayerCallback> &cal
 
 int32_t PlayerImpl::SetParameter(const Format &param)
 {
-    MEDIA_LOGD("PlayerImpl SetParameter in");
+    MEDIA_LOGD("KPI-TRACE: PlayerImpl SetParameter in");
     CHECK_AND_RETURN_RET_LOG(playerService_ != nullptr, MSERR_INVALID_OPERATION, "player service does not exist..");
     return playerService_->SetParameter(param);
 }
