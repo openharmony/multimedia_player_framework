7e15cd63d97f4c5f4f8efb97304f15aac40e7c72
magnificGH <guohui36@huawei.com>
2023-03-24 12:00:44 +0800

    fix avmeta crash
    Signed-off-by: magnificGH <guohui36@huawei.com>

--- services/engine/gstreamer/avmetadatahelper/avmetadatahelper_engine_gst_impl.h
+++ services/engine/gstreamer/avmetadatahelper/avmetadatahelper_engine_gst_impl.h
@@ -50,6 +50,7 @@ private:
         const OutputConfiguration &param, std::vector<std::shared_ptr<AVSharedMemory>> &outFrames);
     int32_t ExtractMetadata();
     void OnNotifyElemSetup(GstElement &elem);
+    void OnNotifyElemUnSetup(GstElement &elem);
     const std::string &GetStatusDescription(OHOS::Media::PlayBinState status);
     void Reset();
 
