93b9afd949a1f332d3d558ad58198a3a22627696
huang-xl <huangxiaolong1@huawei.com>
2023-07-03 19:27:09 +0800

    tdd Signed-off-by: huang-xl<huangxiaolong1@huawei.com>
    Signed-off-by: huang-xl <huangxiaolong1@huawei.com>
    Change-Id: I04a7e81d10be6a99e4ff7245f65ab0fb5bc0f3cf

--- services/engine/gstreamer/player/player_engine_gst_impl.cpp
+++ services/engine/gstreamer/player/player_engine_gst_impl.cpp
@@ -269,7 +269,7 @@ void PlayerEngineGstImpl::HandleBufferingTime(const PlayBinMessage &msg)
             if (bufferingTime_ != mqBufferingTime) {
                 bufferingTime_ = mqBufferingTime;
                 std::shared_ptr<IPlayerEngineObs> notifyObs = obs_.lock();
-                CHECK_AND_RETURN_RET_LOG(notifyObs != nullptr, MSERR_NO_MEMORY, "notifyObs is nullptr");
+                CHECK_AND_RETURN_LOG(notifyObs != nullptr, "notifyObs is nullptr");
                 Format format;
                 (void)format.PutIntValue(std::string(PlayerKeys::PLAYER_CACHED_DURATION),
                     static_cast<int32_t>(mqBufferingTime));
