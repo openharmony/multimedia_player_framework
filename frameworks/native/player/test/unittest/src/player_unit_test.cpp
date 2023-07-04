4e494627e627848515582cbebf04f90237d1e4d6
huang-xl <huangxiaolong1@huawei.com>
2023-07-04 15:11:05 +0800

    tdd Signed-off-by: huang-xl<huangxiaolong1@huawei.com>
    Signed-off-by: huang-xl <huangxiaolong1@huawei.com>
    Change-Id: If56cde9ca5dc5d7333bf791adce5a01cf8921b60

--- frameworks/native/player/test/unittest/src/player_unit_test.cpp
+++ frameworks/native/player/test/unittest/src/player_unit_test.cpp
@@ -169,6 +169,7 @@ HWTEST_F(PlayerUnitTest, Player_SetSource_003, TestSize.Level2)
     EXPECT_NE(720, player_->GetVideoWidth());
     EXPECT_NE(MSERR_OK, player_->Stop());
     EXPECT_EQ(MSERR_OK, player_->Reset());
+    system("param set sys.media.player.buffering.enable FALSE");
 }
 
 /**
