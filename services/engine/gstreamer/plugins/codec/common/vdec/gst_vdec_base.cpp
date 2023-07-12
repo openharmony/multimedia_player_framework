8482ef23cab8de055447ffd6187ec4bd905e52de
lin-jianwu <linjianwu@huawei.com>
2023-07-12 15:07:50 +0800

    fix low framerate can not play end
    Signed-off-by: lin-jianwu <linjianwu@huawei.com>
    Change-Id: I89454a62c8e4123569e4e2eb1749ea437011cfc7

--- services/engine/gstreamer/plugins/codec/common/vdec/gst_vdec_base.cpp
+++ services/engine/gstreamer/plugins/codec/common/vdec/gst_vdec_base.cpp
@@ -35,6 +35,9 @@ GST_DEBUG_CATEGORY_STATIC(gst_vdec_base_debug_category);
 #define DEFAULT_HEIGHT 1080
 #define DEFAULT_SEEK_FRAME_RATE 1000
 #define BLOCKING_ACQUIRE_BUFFER_THRESHOLD 5
+#define DRAIN_TIME_OUT_MIN (G_TIME_SPAN_SECOND * 2)
+#define DRAIN_TIME_OUT_MAX (G_TIME_SPAN_SECOND * 5)
+#define DRAIN_TIME_OUT_BY_FRAMERATE(rate) (static_cast<gint64>(G_TIME_SPAN_SECOND * 30 / (rate)))
 
 static void gst_vdec_base_class_install_property(GObjectClass *gobject_class);
 static void gst_vdec_base_set_property(GObject *object, guint prop_id, const GValue *value, GParamSpec *pspec);
@@ -1243,8 +1246,7 @@ static GstFlowReturn gst_vdec_base_format_change(GstVdecBase *self)
     ON_SCOPE_EXIT(0) { GST_STATE_UNLOCK(self); };
     MediaTrace::TraceBegin("VdecBase::FormatChange", FAKE_POINTER(self));
     GST_WARNING_OBJECT(self, "KPI-TRACE-VDEC: format change start");
-    g_return_val_if_fail(self != nullptr, GST_FLOW_ERROR);
-    g_return_val_if_fail(self->decoder != nullptr, GST_FLOW_ERROR);
+    g_return_val_if_fail(self != nullptr && self->decoder != nullptr, GST_FLOW_ERROR);
     gint ret = self->decoder->ActiveBufferMgr(GST_CODEC_OUTPUT, false);
     g_return_val_if_fail(gst_codec_return_is_ok(self, ret, "ActiveBufferMgr", TRUE), GST_FLOW_ERROR);
     ret = self->decoder->FreeOutputBuffers();
@@ -1553,8 +1555,10 @@ static GstFlowReturn gst_vdec_base_finish(GstVideoDecoder *decoder)
         GST_VIDEO_DECODER_STREAM_LOCK(self);
         return GST_FLOW_ERROR;
     }
-    GST_DEBUG_OBJECT(self, "Waiting until codec is drained");
-    gint64 wait_until = g_get_monotonic_time() + G_TIME_SPAN_SECOND;
+    gint64 wait_time = std::min(DRAIN_TIME_OUT_MAX,
+        std::max(DRAIN_TIME_OUT_MIN, DRAIN_TIME_OUT_BY_FRAMERATE(self->frame_rate)));
+    gint64 wait_until = g_get_monotonic_time() + wait_time;
+    GST_DEBUG_OBJECT(self, "Waiting until codec is drained, wait %" G_GINT64_FORMAT " us", wait_time);
     if (!g_cond_wait_until(&self->drain_cond, &self->drain_lock, wait_until)) {
         GST_ERROR_OBJECT(self, "Drain timed out");
     } else {
