72f895944a0312d1a93d9cf9c98ac262bdb42e08
FileStream <liuwenjian5@huawei.com>
2023-10-19 12:57:16 +0000

    Codex整改
    Signed-off-by: FileStream <liuwenjian5@huawei.com>

--- frameworks/native/soundpool/sound_id_manager.cpp
+++ frameworks/native/soundpool/sound_id_manager.cpp
@@ -144,7 +144,10 @@ int32_t SoundIDManager::DoParser()
         if (soundIDs_.empty()) {
             queueDataValid_.wait_for(
                 lock, std::chrono::duration<int32_t, std::milli>(WAIT_TIME_BEFORE_CLOSE_MS));
-            if (soundIDs_.empty()) break; // no new sound, exit this thread.
+            if (soundIDs_.empty()) {
+                // no new sound, exit this thread.
+                break;
+            }
             continue;
         }
         const int32_t soundID = soundIDs_.front();
