5521810bc3829e9b99012f7deb38ca1bbb4e2d3b
lvjintao <lvjintao1@huawei.com>
2023-10-25 16:23:55 +0800

    fixed e678658 from https://gitee.com/taoljt/multimedia_player_framework/pulls/2281 代码提交
    Signed-off-by: lvjintao <lvjintao1@huawei.com>

--- interfaces/kits/c/avplayer_base.h
+++ interfaces/kits/c/avplayer_base.h
@@ -29,7 +29,7 @@
  *
  * @brief Defines the structure and enumeration for Media AVPlayer.
  *
- * @library libmedia_player.so
+ * @library libavplayer.so
  * @since 11
  * @version 1.0
  */
@@ -50,28 +50,26 @@ typedef struct NativeWindow OHNativeWindow;
  * @since 11
  * @version 1.0
  */
-typedef enum AVPlayerStates {
-    /* error states */
-    AV_STATE_ERROR = 0,
+typedef enum AVPlayerState {
     /* idle states */
-    AV_IDLE = 1,
-    /* initialized states(Internal states) */
-    AV_INITIALIZED = 2,
-    /* preparing states(Internal states) */
-    AV_PREPARING = 3,
+    AV_IDLE = 0,
+    /* initialized states */
+    AV_INITIALIZED = 1,
     /* prepared states */
-    AV_PREPARED = 4,
-    /* started states */
-    AV_STARTED = 5,
+    AV_PREPARED = 2,
+    /* playing states */
+    AV_PLAYING = 3,
     /* paused states */
-    AV_PAUSED = 6,
+    AV_PAUSED = 4,
     /* stopped states */
-    AV_STOPPED = 7,
+    AV_STOPPED = 5,
     /* Play to the end states */
-    AV_PLAYBACK_COMPLETE = 8,
+    AV_COMPLETED = 6,
     /* released states */
-    AV_RELEASED = 9,
-} AVPlayerStates;
+    AV_RELEASED = 7,
+    /* error states */
+    AV_ERROR = 8,
+} AVPlayerState;
 
 /**
  * @brief Player Seek Mode
@@ -84,19 +82,15 @@ typedef enum AVPlayerSeekMode {
     AV_SEEK_NEXT_SYNC = 0,
     /* sync to keyframes before the time point. */
     AV_SEEK_PREVIOUS_SYNC,
-    /* sync to closest keyframes. */
-    AV_SEEK_CLOSEST_SYNC,
-    /* seek to frames closest the time point. */
-    AV_SEEK_CLOSEST,
 } AVPlayerSeekMode;
 
 /**
- * @brief Playback Rate Mode
+ * @brief Playback Speed
  * @syscap SystemCapability.Multimedia.Media.AVPlayer
  * @since 11
  * @version 1.0
  */
-typedef enum AVPlaybackRateMode {
+typedef enum AVPlaybackSpeed {
     /* Video playback at 0.75x normal speed */
     AV_SPEED_FORWARD_0_75_X,
     /* Video playback at normal speed */
@@ -107,7 +101,7 @@ typedef enum AVPlaybackRateMode {
     AV_SPEED_FORWARD_1_75_X,
     /* Video playback at 2.0x normal speed */
     AV_SPEED_FORWARD_2_00_X,
-} AVPlaybackRateMode;
+} AVPlaybackSpeed;
 
 /**
  * @brief Player OnInfo Type
@@ -117,55 +111,41 @@ typedef enum AVPlaybackRateMode {
  */
 typedef enum AVPlayerOnInfoType {
     /* return the message when seeking done. */
-    AV_INFO_TYPE_SEEKDONE = 1,
+    AV_INFO_TYPE_SEEKDONE = 0,
     /* return the message when speeding done. */
-    AV_INFO_TYPE_SPEEDDONE,
+    AV_INFO_TYPE_SPEEDDONE = 1,
     /* return the message when select bitrate done */
-    AV_INFO_TYPE_BITRATEDONE,
+    AV_INFO_TYPE_BITRATEDONE = 2,
     /* return the message when playback is end of steam. */
-    AV_INFO_TYPE_EOS,
+    AV_INFO_TYPE_EOS = 3,
     /* return the message when PlayerStates changed. */
-    AV_INFO_TYPE_STATE_CHANGE,
+    AV_INFO_TYPE_STATE_CHANGE = 4,
     /* return the current posion of playback automatically. */
-    AV_INFO_TYPE_POSITION_UPDATE,
+    AV_INFO_TYPE_POSITION_UPDATE = 5,
     /* return the playback message. */
-    AV_INFO_TYPE_MESSAGE,
+    AV_INFO_TYPE_MESSAGE = 6,
     /* return the message when volume changed. */
-    AV_INFO_TYPE_VOLUME_CHANGE,
+    AV_INFO_TYPE_VOLUME_CHANGE = 7,
     /* return the message when video size is first known or updated. */
-    AV_INFO_TYPE_RESOLUTION_CHANGE,
+    AV_INFO_TYPE_RESOLUTION_CHANGE = 8,
     /* return multiqueue buffering time. */
-    AV_INFO_TYPE_BUFFERING_UPDATE,
+    AV_INFO_TYPE_BUFFERING_UPDATE = 9,
     /* return hls bitrate.
        Bitrate is to convert data into uint8_t array storage,
        which needs to be forcibly converted to uint32_t through offset access. */
-    AV_INFO_TYPE_BITRATE_COLLECT,
+    AV_INFO_TYPE_BITRATE_COLLECT = 10,
     /* return the message when audio focus changed. */
-    AV_INFO_TYPE_INTERRUPT_EVENT,
-    /* return the message when PlayerStates changed by audio. */
-    AV_INFO_TYPE_STATE_CHANGE_BY_AUDIO,
-    /* return the message with extra information in format. */
-    AV_INFO_TYPE_EXTRA_FORMAT,
+    AV_INFO_TYPE_INTERRUPT_EVENT = 11,
     /* return the duration of playback. */
-    AV_INFO_TYPE_DURATION_UPDATE,
+    AV_INFO_TYPE_DURATION_UPDATE = 12,
     /* return the playback is live stream. */
-    AV_INFO_TYPE_IS_LIVE_STREAM,
+    AV_INFO_TYPE_IS_LIVE_STREAM = 13,
     /* return the message when track changes. */
-    AV_INFO_TYPE_TRACKCHANGE,
-    /* return the default audio track. */
-    INFO_TYPE_DEFAULTTRACK,
-    /* return to the end of track processing. */
-    AV_INFO_TYPE_TRACK_DONE,
-    /* return error message to prompt the user. */
-    AV_INFO_TYPE_ERROR_MSG,
-    /* return the message when subtitle track num updated. */
-    AV_INFO_TYPE_TRACK_NUM_UPDATE,
+    AV_INFO_TYPE_TRACKCHANGE = 14,
     /* return the message when subtitle track info updated. */
-    AV_INFO_TYPE_TRACK_OH_INFO_UPDATE,
+    AV_INFO_TYPE_TRACK_INFO_UPDATE = 15,
     /* return the subtitle of playback. */
-    AV_INFO_TYPE_SUBTITLE_UPDATE,
-    /* return to the end of adding subtitle processing. */
-    AV_INFO_TYPE_ADD_SUBTITLE_DONE,
+    AV_INFO_TYPE_SUBTITLE_UPDATE = 16,
 } AVPlayerOnInfoType;
 
 /**
