6541c42ffe85b03b93ac382a5262a7cec1f8311f
zhangever <zhangyue57@huawei.com>
2023-12-25 08:25:54 +0000

    update frameworks/js/avrecorder/avrecorder_napi.cpp.
    Signed-off-by: zhangever <zhangyue57@huawei.com>

--- frameworks/js/avrecorder/avrecorder_napi.cpp
+++ frameworks/js/avrecorder/avrecorder_napi.cpp
@@ -1037,11 +1037,13 @@ int32_t AVRecorderNapi::GetProfile(std::unique_ptr<AVRecorderAsyncContext> &asyn
             (asyncCtx->AVRecorderSignError(ret, "GetvideoFrameHeight", "videoFrameHeight"), ret));
         CHECK_AND_RETURN_RET(CommonNapi::GetPropertyInt32(env, item, "videoFrameRate", profile.videoFrameRate),
             (asyncCtx->AVRecorderSignError(ret, "GetvideoFrameRate", "videoFrameRate"), ret));
-        CHECK_AND_RETURN_RET(CommonNapi::GetPropertyBool(env, item, "isHdr", profile.isHdr),
-            (asyncCtx->AVRecorderSignError(ret, "GetIsHdr", "isHdr"), ret));
-        CHECK_AND_RETURN_RET(!(profile.isHdr && (profile.videoCodecFormat != VideoCodecFormat::H265)),
-            (asyncCtx->AVRecorderSignError(MSERR_UNSUPPORT_VID_PARAMS, "isHdr needs to match video/hevc", ""),
-            MSERR_UNSUPPORT_VID_PARAMS));
+        if (CommonNapi::GetPropertyBool(env, item, "isHdr", profile.isHdr)) {
+            CHECK_AND_RETURN_RET(!(profile.isHdr && (profile.videoCodecFormat != VideoCodecFormat::H265)),
+                (asyncCtx->AVRecorderSignError(MSERR_UNSUPPORT_VID_PARAMS, "isHdr needs to match video/hevc", ""),
+                MSERR_UNSUPPORT_VID_PARAMS));
+        } else {
+            profile.isHdr = false;
+        }
         MediaProfileLog(true, profile);
     }
 
