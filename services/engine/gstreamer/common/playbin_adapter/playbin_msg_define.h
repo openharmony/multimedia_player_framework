feaf29134dba1a37e4d5b824afeb8455ca16fad4
songshenke <songshenke@huawei.com>
2023-12-30 16:05:11 +0800

    Add the first frame callback for AudioHaptic
    Signed-off-by: songshenke <songshenke@huawei.com>
    Change-Id: I97ef5950519f90b5d4c9690073baed40f65812c0

--- services/engine/gstreamer/common/playbin_adapter/playbin_msg_define.h
+++ services/engine/gstreamer/common/playbin_adapter/playbin_msg_define.h
@@ -78,6 +78,7 @@ enum PlayBinMsgSubType : int32_t {
 
 enum PlayBinMsgAudioSink : int32_t {
     PLAYBIN_MSG_INTERRUPT_EVENT,
+    PLAYBIN_MSG_FIRST_FRAME_EVENT,
 };
 
 enum PlayBinState : int32_t {
