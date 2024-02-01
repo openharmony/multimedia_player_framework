34538f837531440aa99fd44300d0116a63dfc875
l30052684 <linxinjian1@huawei.com>
2024-02-01 17:17:32 +0800

    fix system_tone_play_napi
    Signed-off-by: l30052684 <linxinjian1@huawei.com>
    Change-Id: I4e2710d29e4f5f2dc736000e743d9164b526abf3

--- frameworks/js/common/common_napi.h
+++ frameworks/js/common/common_napi.h
@@ -295,6 +295,93 @@ struct AVDataSrcDescriptor {
     int64_t fileSize = 0;
     napi_value callback = nullptr;
 };
+
+template<typename T>
+class ObjectRefMap {
+public:
+    static std::mutex allObjLock;
+    static std::map<T*, uint32_t> refMap;
+    static void Insert(T *obj);
+    static void Erase(T *obj);
+    static T *IncreaseRef(T *obj);
+    static void DecreaseRef(T *obj);
+
+    ObjectRefMap(T *obj);
+    ~ObjectRefMap();
+    T *GetPtr();
+
+private:
+    T *obj_ = nullptr;
+};
+
+template <typename T>
+std::mutex ObjectRefMap<T>::allObjLock;
+
+template <typename T>
+std::map<T *, uint32_t> ObjectRefMap<T>::refMap;
+
+template <typename T>
+void ObjectRefMap<T>::Insert(T *obj)
+{
+    std::lock_guard<std::mutex> lock(allObjLock);
+    refMap[obj] = 1;
+}
+
+template <typename T>
+void ObjectRefMap<T>::Erase(T *obj)
+{
+    std::lock_guard<std::mutex> lock(allObjLock);
+    auto it = refMap.find(obj);
+    if (it != refMap.end()) {
+        refMap.erase(it);
+    }
+}
+
+template <typename T>
+T *ObjectRefMap<T>::IncreaseRef(T *obj)
+{
+    std::lock_guard<std::mutex> lock(allObjLock);
+    if (refMap.count(obj)) {
+        refMap[obj]++;
+        return obj;
+    } else {
+        return nullptr;
+    }
+}
+
+template <typename T>
+void ObjectRefMap<T>::DecreaseRef(T *obj)
+{
+    std::lock_guard<std::mutex> lock(allObjLock);
+    if (refMap.count(obj) && --refMap[obj] == 0) {
+        refMap.erase(obj);
+        delete obj;
+        obj = nullptr;
+    }
+}
+
+template <typename T>
+ObjectRefMap<T>::ObjectRefMap(T *obj)
+{
+    if (obj != nullptr) {
+        obj_ = ObjectRefMap::IncreaseRef(obj);
+    }
+}
+
+template <typename T>
+ObjectRefMap<T>::~ObjectRefMap()
+{
+    if (obj_ != nullptr) {
+        ObjectRefMap::DecreaseRef(obj_);
+    }
+}
+
+template <typename T>
+T *ObjectRefMap<T>::GetPtr()
+{
+    return obj_;
+}
+
 } // namespace Media
 } // namespace OHOS
 #endif // COMMON_NAPI_H
