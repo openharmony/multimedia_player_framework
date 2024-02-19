196f7c1f75176f9f87e252985591d50f47b37dc5
王海州 <wanghaizhou6@huawei.com>
2024-02-19 08:45:21 +0000

    fixed eb750bc from https://gitee.com/kelvin-xie/multimedia_player_framework/pulls/2782 seek fix
    Signed-off-by: 王海州 <wanghaizhou6@huawei.com>

--- services/engine/histreamer/player/hiplayer_impl.h
+++ services/engine/histreamer/player/hiplayer_impl.h
@@ -130,7 +130,6 @@ private:
     Status doPausedSeek(int64_t seekPos, PlayerSeekMode mode);
     Status doCompletedSeek(int64_t seekPos, PlayerSeekMode mode);
     Status doSeek(int64_t seekPos, PlayerSeekMode mode);
-    void NotifySeek(Status rtv, bool flag, int64_t seekPos);
     int32_t PauseInner();
 
     bool isNetWorkPlay_ = false;
