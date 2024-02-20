5e8594b65393851e65a487dbcb9af8092f04c41c
韩汶钊 <hanwenzhao@huawei.com>
2024-02-20 14:11:29 +0000

    update services/engine/histreamer/avmetadatahelper/avmetadatahelper_impl.cpp.
    Signed-off-by: 韩汶钊 <hanwenzhao@huawei.com>

--- services/engine/histreamer/avmetadatahelper/avmetadatahelper_impl.cpp
+++ services/engine/histreamer/avmetadatahelper/avmetadatahelper_impl.cpp
@@ -163,10 +163,10 @@ static void ConvertP010ToNV12(const sptr<SurfaceBuffer> &surfaceBuffer, uint8_t 
 
     // copy src Y component to dst
     for (int32_t i = 0; i < height; i++) {
-        uint16_t *srcY = reinterpret_cast<uint16_t *>(srcP010 + strideWidth * i);
+        uint16_t *srcY = (uint16_t *)(srcP010 + strideWidth * i);
         uint8_t *dstY = dstNV12 + width * i;
         for (int32_t j = 0; j < width; j++) {
-            *dstY = static_cast<uint8_t>(*srcY >> SHIFT_BITS_P010_2_NV12);
+            *dstY = (uint8_t)(*srcY >> SHIFT_BITS_P010_2_NV12);
             srcY++;
             dstY++;
         }
@@ -181,11 +181,11 @@ static void ConvertP010ToNV12(const sptr<SurfaceBuffer> &surfaceBuffer, uint8_t 
 
     // copy src UV component to dst, height(UV) = height(Y) / 2;
     for (int32_t i = 0; i < height / 2; i++) {
-        uint16_t *srcUV = reinterpret_cast<uint16_t *>(srcP010 + strideWidth * i);
+        uint16_t *srcUV = (uint16_t *)(srcP010 + strideWidth * i);
         uint8_t *dstUV = dstNV12 + width * i;
         for (int32_t j = 0; j < width && srcUV < maxSrcAddr && dstUV < maxDstAddr; j++) {
-            *dstUV = static_cast<uint8_t>(*srcUV >> SHIFT_BITS_P010_2_NV12);
-            *(dstUV + 1) = static_cast<uint8_t>(*(srcUV + 1) >> SHIFT_BITS_P010_2_NV12);
+            *dstUV = (uint8_t)(*srcUV >> SHIFT_BITS_P010_2_NV12);
+            *(dstUV + 1) = (uint8_t)(*(srcUV + 1) >> SHIFT_BITS_P010_2_NV12);
             srcUV += 2; // srcUV move by 2 to process U and V component
             dstUV += 2; // dstUV move by 2 to process U and V component
         }
