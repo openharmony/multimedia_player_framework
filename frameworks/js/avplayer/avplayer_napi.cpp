8be045a9ca3831be5de0f2e1b0cd7a8d0b2a76cb
liyang <liyang782@huawei.com>
2024-02-26 22:27:20 +0800

    app freeze issue fix
    Signed-off-by: liyang <liyang782@huawei.com>

--- frameworks/js/avplayer/avplayer_napi.cpp
+++ frameworks/js/avplayer/avplayer_napi.cpp
@@ -617,7 +617,7 @@ std::shared_ptr<TaskHandler<TaskRet>> AVPlayerNapi::ReleaseTask()
         isReleased_.store(true);
         (void)taskQue_->EnqueueTask(task, true); // CancelNotExecutedTask
         if (taskQue_->IsTaskExecuting()) {
-            MEDIA_LOGW("0x%{public}06" PRIXPTR " Cancel Executing Task,ReleaseTask Report Error, FAKE_POINTER(this));
+            MEDIA_LOGW("0x%{public}06" PRIXPTR " Cancel Executing Task,ReleaseTask Report Error", FAKE_POINTER(this));
             NotifyState(PlayerStates::PLAYER_STATE_ERROR);
         }
     }
