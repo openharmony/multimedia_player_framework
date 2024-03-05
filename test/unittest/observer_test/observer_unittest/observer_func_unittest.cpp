004fd586cd712ff8d30bb582599b76239d32ed75
liyang <liyang782@huawei.com>
2024-03-05 22:37:32 +0800

    Add Screen Capture In Call Observer Functions
    Signed-off-by: liyang <liyang782@huawei.com>

---
+++ test/unittest/observer_test/observer_unittest/observer_func_unittest.cpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <string>
+#include "incall_observer.h"
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+using namespace testing::ext;
+using namespace OHOS;
+using namespace OHOS::Media;
+
+namespace OHOS {
+namespace Media {
+namespace InCallObserverFuncUT {
+class InCallObserverInnerUnitTest : public testing::Test {
+public:
+    static void SetUpTestCase(void);
+
+    static void TearDownTestCase(void);
+
+    void SetUp(void);
+
+    void TearDown(void);
+
+};
+
+void InCallObserverInnerUnitTest::SetUpTestCase(void) {}
+
+void InCallObserverInnerUnitTest::TearDownTestCase(void) {}
+
+void InCallObserverInnerUnitTest::SetUp(void)
+{
+    std::cout << "[SetUp]: SetUp!!!, test: ";
+    const ::testing::TestInfo *testInfo_ = ::testing::UnitTest::GetInstance()->current_test_info();
+    std::string testName = testInfo_->name();
+    std::cout << testName << std::endl;
+}
+
+void InCallObserverInnerUnitTest::TearDown(void)
+{
+    std::cout << "[TearDown]: over!!!" << std::endl;
+}
+
+/**
+ * @tc.name: RegisterObserver_01
+ * @tc.desc: RegisterObserver_01
+ * @tc.type: FUNC
+ */
+HWTEST_F(InCallObserverInnerUnitTest, RegisterObserver_01, TestSize.Level1)
+{
+    InCallObserver::GetInstance().RegisterObserver();
+}
+
+/**
+ * @tc.name: OnCallStateUpdated_01
+ * @tc.desc: OnCallStateUpdated_01
+ * @tc.type: FUNC
+ */
+    HWTEST_F(InCallObserverInnerUnitTest, OnCallStateUpdated_01, TestSize.Level1)
+{
+    ASSERT_FALSE(InCallObserver::GetInstance().IsInCall());
+    InCallObserver::GetInstance().OnCallStateUpdated(true);
+    ASSERT_TRUE(InCallObserver::GetInstance().IsInCall());
+}
+} // namespace MetaFuncUT
+} // namespace Media
+} // namespace OHOS
