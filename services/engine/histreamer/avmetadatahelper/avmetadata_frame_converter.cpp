68a309171e49d955c7dd0abef99f100eaab759ac
韩汶钊 <hanwenzhao@huawei.com>
2024-03-06 14:23:01 +0000

    update services/engine/histreamer/avmetadatahelper/avmetadata_frame_converter.cpp.
    Signed-off-by: 韩汶钊 <hanwenzhao@huawei.com>

--- services/engine/histreamer/avmetadatahelper/avmetadata_frame_converter.cpp
+++ services/engine/histreamer/avmetadatahelper/avmetadata_frame_converter.cpp
@@ -64,7 +64,7 @@ std::unique_ptr<PixelMap> AVMetadataFrameConverter::RGBxToRGB565(const std::uniq
         }
     }
     return dstPixelMap;
-} 
+}
 
 std::unique_ptr<PixelMap> AVMetadataFrameConverter::RGBxToRGB888(const std::unique_ptr<PixelMap> &srcPixelMap)
 {
