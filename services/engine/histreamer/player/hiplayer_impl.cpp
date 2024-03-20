153bdc081aa9e9b261c2868a6b865e6516ad0454
laamy <liuao25@huawei.com>
2024-03-20 15:41:51 +0800

    complete not allow pause
    Signed-off-by: laamy <liuao25@huawei.com>

--- services/engine/histreamer/player/hiplayer_impl.cpp
+++ services/engine/histreamer/player/hiplayer_impl.cpp
@@ -333,10 +333,10 @@ int32_t HiPlayerImpl::Play()
 int32_t HiPlayerImpl::Pause()
 {
     MediaTrace trace("HiPlayerImpl::Pause");
+    MEDIA_LOG_I("Pause entered.");
     if (pipelineStates_ == PlayerStates::PLAYER_PLAYBACK_COMPLETE) {
         return TransStatus(Status::OK);
     }
-    MEDIA_LOG_I("Pause entered.");
     if (audioSink_ != nullptr) {
         audioSink_->SetVolumeWithRamp(MIN_MEDIA_VOLUME, FADE_OUT_LATENCY);
     }
