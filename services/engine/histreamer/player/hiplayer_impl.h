clipsheep6/multimedia_player_framework_OH_fork

New Feature.支持切换多音轨
Signed-off-by: caochuan <caochuan@huawei.com>
Change-Id: I883057b87b2dac9c21b3364252a19b1453b29a22

services/engine/histreamer/player/hiplayer_impl.h
@@ -73,6 +73,8 @@ public:
     int32_t GetAudioEffectMode(int32_t &effectMode) override;
     int32_t SetAudioEffectMode(int32_t effectMode) override;
 
+    int32_t SelectTrack(int32_t trackId)  override;
+    int32_t DeselectTrack(int32_t trackId)  override;
     int32_t GetVideoTrackInfo(std::vector<Format>& videoTrack) override;
     int32_t GetAudioTrackInfo(std::vector<Format>& audioTrack) override;
     int32_t GetVideoWidth() override;
@@ -206,6 +208,7 @@ private:
     bool preferHDR_ = false;
     bool isInCompleted_ {false};
     std::string playerId_;
+    uint32_t currentAudioTrackId_ = -1;
 };
 } // namespace Media
 } // namespace OHOS

openharmony/multimedia_player_framework

services/engine/histreamer/player/hiplayer_impl.h
@@ -74,6 +74,9 @@ public:
     int32_t GetAudioEffectMode(int32_t &effectMode) override;
     int32_t SetAudioEffectMode(int32_t effectMode) override;
 
+    int32_t GetCurrentTrack(int32_t trackType, int32_t &index) override;
+    int32_t SelectTrack(int32_t trackId) override;
+    int32_t DeselectTrack(int32_t trackId) override;
     int32_t GetVideoTrackInfo(std::vector<Format>& videoTrack) override;
     int32_t GetAudioTrackInfo(std::vector<Format>& audioTrack) override;
     int32_t GetVideoWidth() override;
@@ -143,6 +146,7 @@ private:
     int32_t InitDuration();
     int32_t InitVideoWidthAndHeight();
     void SetBundleName(std::string bundleName);
+    void InitAudioDefaultTrackIndex();
 
     bool isNetWorkPlay_ = false;
     int32_t appUid_{0};
@@ -208,6 +212,8 @@ private:
     bool preferHDR_ = false;
     bool isInCompleted_ {false};
     std::string playerId_;
+    int32_t currentAudioTrackId_ = -1;
+    int32_t defaultAudioTrackId_ = -1;
 };
 } // namespace Media
 } // namespace OHOS