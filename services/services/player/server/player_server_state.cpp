c61a48759d76bccf8e9c5305d4d4d4c19e9b3b73
fanpingan <fanpingan1@huawei.com>
2024-05-27 19:31:22 +0800

    Bugfix qingtingFM high power consumption because of player idle running
    Signed-off-by: fanpingan <fanpingan1@huawei.com>
    Change-Id: I6bfbebf08bb9880e948dcf9b5e68c1eac3ef1eea

--- services/services/player/server/player_server_state.cpp
+++ services/services/player/server/player_server_state.cpp
@@ -248,7 +248,6 @@ void PlayerServer::PreparedState::HandleStateChange(int32_t newState)
         server_.lastOpStatue_ = PLAYER_STATE_ERROR;
         server_.ChangeState(server_.initializedState_);
         (void)server_.taskMgr_.MarkTaskDone("prepared->error done");
-        
     }
 }
 
