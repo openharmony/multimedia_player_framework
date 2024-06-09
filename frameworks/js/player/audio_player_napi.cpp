1763dc23646d6beafa25542f9eff2bfda9712117
liyikang <liyikang@h-partners.com>
2024-06-09 09:05:34 +0000

    url日志打印整改audio_player_napi追加
    Signed-off-by: liyikang <liyikang@h-partners.com>

--- frameworks/js/player/audio_player_napi.cpp
+++ frameworks/js/player/audio_player_napi.cpp
@@ -230,7 +230,6 @@ napi_value AudioPlayerNapi::SetSrc(napi_env env, napi_callback_info info)
     const std::string httpHead = "http";
     int32_t ret = MSERR_EXT_INVALID_VAL;
 
-    MEDIA_LOGD("input url is %{public}s!", player->uri_.c_str());
     if (player->uri_.find(fdHead) != std::string::npos) {
         int32_t fd = -1;
         std::string inputFd = player->uri_.substr(fdHead.size());
