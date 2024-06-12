c90945ef65c8607f58a60a3407426f9bd82cf67b
zhaijunwei <zhaijunwei123@huawei.com>
2024-06-12 15:07:17 +0800

    modify about openfile
    Signed-off-by: zhaijunwei <zhaijunwei123@huawei.com>

--- frameworks/native/system_sound_manager/system_sound_manager_impl.cpp
+++ frameworks/native/system_sound_manager/system_sound_manager_impl.cpp
@@ -900,9 +900,14 @@ std::string SystemSoundManagerImpl::AddCustomizedToneByFdAndOffset(
     std::lock_guard<std::mutex> lock(uriMutex_);
     int32_t srcFd = fd;
     off_t lseekResult = lseek(srcFd, offset, SEEK_SET);
+    if (srcFd < 0 || lseekResult == -1) {
+        MEDIA_LOGE("fd is error");
+        result.clear();
+        return result;
+    }
     std::shared_ptr<DataShare::DataShareHelper> dataShareHelper = CreateDataShareHelper(STORAGE_MANAGER_MANAGER_ID);
-    if (dataShareHelper == nullptr || srcFd < 0 || lseekResult == -1) {
-        MEDIA_LOGE("dataShareHelper is nullptr or fd is error");
+    if (dataShareHelper == nullptr) {
+        MEDIA_LOGE("dataShareHelper is nullptr");
         result.clear();
         return result;
     }
