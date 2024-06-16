7565ba59d4470fbe3548857dfabad671660581e6
fanpingan <fanpingan1@huawei.com>
2024-06-16 03:51:08 +0000

    update services/services/player/server/player_server.h.
    Signed-off-by: fanpingan <fanpingan1@huawei.com>

--- services/services/player/server/player_server.h
+++ services/services/player/server/player_server.h
@@ -175,7 +175,7 @@ protected:
         std::string url;
         int32_t effectMode = OHOS::AudioStandard::AudioEffectMode::EFFECT_DEFAULT;
         std::map<std::string, std::string> header;
-        AVPlayStrategy strategy_ = nullptr;
+        AVPlayStrategy strategy_;
     } config_;
 
 private:
