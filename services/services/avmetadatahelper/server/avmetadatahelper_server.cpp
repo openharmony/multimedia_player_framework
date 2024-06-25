13d752114cb3b0a01ccead7f167643becb3bf890
hanwenzhao <hanwenzhao@huawei.com>
2024-06-25 20:46:06 +0800

    fix wrong code while reducing avmeta thread
    Signed-off-by: hanwenzhao <hanwenzhao@huawei.com>
    Change-Id: I310949719dbf8d57d9c08835bcf15c8e3d1548a6

--- services/services/avmetadatahelper/server/avmetadatahelper_server.cpp
+++ services/services/avmetadatahelper/server/avmetadatahelper_server.cpp
@@ -79,8 +79,7 @@ int32_t AVMetadataHelperServer::SetSource(const std::string &uri, int32_t usage)
     CHECK_AND_RETURN_RET_LOG(avMetadataHelperEngine_ != nullptr,
         (int32_t)MSERR_CREATE_AVMETADATAHELPER_ENGINE_FAILED, "Failed to create avmetadatahelper engine.");
     int32_t ret = avMetadataHelperEngine_->SetSource(uriHelper_->FormattedUri(), usage);
-    CHECK_AND_RETURN_RET_LOG(ret == MSERR_OK, ret, "0x%{public}06" PRIXPTR " SetSource failed", FAKE_POINTER(this));
-    ChangeState(HelperStates::HELPER_PREPARED);
+    ChangeState(ret == MSERR_OK ? HelperStates::HELPER_PREPARED : HelperStates::HELPER_STATE_ERROR);
     return ret;
 }
 
@@ -93,21 +92,13 @@ int32_t AVMetadataHelperServer::SetSource(int32_t fd, int64_t offset, int64_t si
 
     uriHelper_ = std::make_unique<UriHelper>(fd, offset, size);
     CHECK_AND_RETURN_RET_LOG(!uriHelper_->FormattedUri().empty(),
-                             MSERR_INVALID_VAL,
-                             "Failed to construct formatted uri");
+        MSERR_INVALID_VAL, "Failed to construct formatted uri");
     CHECK_AND_RETURN_RET_LOG(uriHelper_->AccessCheck(UriHelper::URI_READ), MSERR_INVALID_VAL, "Failed to read the fd");
-
-    auto engineFactory = EngineFactoryRepo::Instance().GetEngineFactory(
-        IEngineFactory::Scene::SCENE_AVMETADATA, appUid_, uriHelper_->FormattedUri());
-    CHECK_AND_RETURN_RET_LOG(engineFactory != nullptr, (int32_t)MSERR_CREATE_AVMETADATAHELPER_ENGINE_FAILED,
-        "Failed to get engine factory");
-    avMetadataHelperEngine_ = engineFactory->CreateAVMetadataHelperEngine();
+    InitEngine(uriHelper_->FormattedUri());
     CHECK_AND_RETURN_RET_LOG(avMetadataHelperEngine_ != nullptr,
         (int32_t)MSERR_CREATE_AVMETADATAHELPER_ENGINE_FAILED, "Failed to create avmetadatahelper engine");
 
     int32_t ret = avMetadataHelperEngine_->SetSource(uriHelper_->FormattedUri(), usage);
-    CHECK_AND_RETURN_RET_LOG(ret == MSERR_OK, ret, "0x%{public}06" PRIXPTR " SetSource failed!",
-        FAKE_POINTER(this));
     ChangeState(ret == MSERR_OK ? HelperStates::HELPER_PREPARED : HelperStates::HELPER_STATE_ERROR);
     return ret;
 }
@@ -119,12 +110,7 @@ int32_t AVMetadataHelperServer::SetSource(const std::shared_ptr<IMediaDataSource
     MEDIA_LOGD("AVMetadataHelperServer SetSource");
     CHECK_AND_RETURN_RET_LOG(dataSrc != nullptr, MSERR_INVALID_VAL, "data source is nullptr");
     dataSrc_ = dataSrc;
-    std::string url = "media data source";
-    auto engineFactory = EngineFactoryRepo::Instance().GetEngineFactory(
-        IEngineFactory::Scene::SCENE_AVMETADATA, appUid_, url);
-    CHECK_AND_RETURN_RET_LOG(engineFactory != nullptr, (int32_t)MSERR_CREATE_AVMETADATAHELPER_ENGINE_FAILED,
-        "Failed to get engine factory");
-    avMetadataHelperEngine_ = engineFactory->CreateAVMetadataHelperEngine();
+    InitEngine("media data source");
     CHECK_AND_RETURN_RET_LOG(avMetadataHelperEngine_ != nullptr,
         (int32_t)MSERR_CREATE_AVMETADATAHELPER_ENGINE_FAILED, "Failed to create avmetadatahelper engine");
 
@@ -132,6 +118,15 @@ int32_t AVMetadataHelperServer::SetSource(const std::shared_ptr<IMediaDataSource
     CHECK_AND_RETURN_RET_LOG(ret == MSERR_OK, ret, "SetSource failed!");
     ChangeState(HelperStates::HELPER_PREPARED);
     return ret;
+}
+
+int32_t AVMetadataHelperServer::InitEngine(const std::string &uri)
+{
+    auto engineFactory = EngineFactoryRepo::Instance().GetEngineFactory(
+        IEngineFactory::Scene::SCENE_AVMETADATA, appUid_, uri);
+    CHECK_AND_RETURN_RET_LOG(engineFactory != nullptr, (int32_t)MSERR_CREATE_AVMETADATAHELPER_ENGINE_FAILED,
+        "Failed to get engine factory");
+    avMetadataHelperEngine_ = engineFactory->CreateAVMetadataHelperEngine();
 }
 
 std::string AVMetadataHelperServer::ResolveMetadata(int32_t key)
