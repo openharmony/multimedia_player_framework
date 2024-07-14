d18543d61a0b2e234f83f7af03239f18007444ba
韩汶钊 <hanwenzhao@huawei.com>
2024-07-14 14:50:53 +0000

    蓝黄同步
    Signed-off-by: 韩汶钊 <hanwenzhao@huawei.com>

--- services/services/recorder/ipc/recorder_listener_proxy.cpp
+++ services/services/recorder/ipc/recorder_listener_proxy.cpp
@@ -13,14 +13,12 @@
  * limitations under the License.
  */
 
-#define PLAYER_FRAMEWORK_RECORDER
-
 #include "recorder_listener_proxy.h"
 #include "media_log.h"
 #include "media_errors.h"
 
 namespace {
-constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, LOG_DOMAIN, "RecorderListenerProxy"};
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, LOG_DOMAIN_RECORDER, "RecorderListenerProxy"};
 }
 
 namespace OHOS {
