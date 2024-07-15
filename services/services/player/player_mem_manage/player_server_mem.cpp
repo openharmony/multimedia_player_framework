4c8ad6046077e38b7804038777af3735eee1317a
assertsu <subenhui@huawei.com>
2024-07-15 10:03:35 +0000

    add 1
    Signed-off-by: assertsu <subenhui@huawei.com>

--- services/services/player/player_mem_manage/player_server_mem.cpp
+++ services/services/player/player_mem_manage/player_server_mem.cpp
@@ -189,7 +189,7 @@ int32_t PlayerServerMem::SetBehaviorInternal()
     }
 
     if (NeedSelectAudioTrack()) {
-        ret = PlayerServer::SelectTrack(recoverConfig_.audioIndex);
+        ret = PlayerServer::SelectTrack(recoverConfig_.audioIndex, PlayerSwitchMode::SWITCH_SOOMTH);
         CHECK_AND_RETURN_RET_LOG(ret == MSERR_OK, MSERR_INVALID_OPERATION, "failed to SelectTrack");
     }
 
@@ -206,7 +206,7 @@ int32_t PlayerServerMem::SetPlaybackSpeedInternal()
     }
 
     if (NeedSelectAudioTrack()) {
-        ret = PlayerServer::SelectTrack(recoverConfig_.audioIndex);
+        ret = PlayerServer::SelectTrack(recoverConfig_.audioIndex, PlayerSwitchMode::SWITCH_SOOMTH);
         CHECK_AND_RETURN_RET_LOG(ret == MSERR_OK, MSERR_INVALID_OPERATION, "failed to SelectTrack");
     }
     return MSERR_OK;
@@ -589,13 +589,13 @@ int32_t PlayerServerMem::SelectBitRate(uint32_t bitRate)
     return PlayerServer::SelectBitRate(bitRate);
 }
 
-int32_t PlayerServerMem::SelectTrack(int32_t index)
+int32_t PlayerServerMem::SelectTrack(int32_t index, PlayerSwitchMode mode)
 {
     std::unique_lock<std::mutex> lock(mutex_);
     CHECK_AND_RETURN_RET_LOG(RecoverMemByUser() == MSERR_OK, MSERR_INVALID_OPERATION, "RecoverMemByUser fail");
 
     lastestUserSetTime_ = std::chrono::steady_clock::now();
-    return PlayerServer::SelectTrack(index);
+    return PlayerServer::SelectTrack(index, mode);
 }
 
 int32_t PlayerServerMem::DeselectTrack(int32_t index)
