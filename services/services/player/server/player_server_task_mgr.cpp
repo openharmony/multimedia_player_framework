b5f2840e6e25703abfd86cd655e4c73231df44a7
道常参商 <yangzhenjun2@huawei.com>
2024-07-26 07:35:20 +0000

    update services/services/player/server/player_server_task_mgr.cpp.
    Signed-off-by: 道常参商 <yangzhenjun2@huawei.com>

--- services/services/player/server/player_server_task_mgr.cpp
+++ services/services/player/server/player_server_task_mgr.cpp
@@ -88,17 +88,17 @@ int32_t PlayerServerTaskMgr::LaunchTask(const std::shared_ptr<ITaskHandler> &tas
     }
     if (currTwoPhaseTask_ == nullptr) {
         return EnqueueTask(task, type, taskName);
-    } else {
-        if (taskName.compare("volume") == 0) {
-            MEDIA_LOGD("0x%{public}06" PRIXPTR " task[%{public}s] is in processing, the new task[%{public}s]",
-                FAKE_POINTER(this), currTwoPhaseTaskName_.c_str(), taskName.c_str());
-        } else {
-            MEDIA_LOGI("0x%{public}06" PRIXPTR " task[%{public}s] is in processing, the new task[%{public}s]",
-                FAKE_POINTER(this), currTwoPhaseTaskName_.c_str(), taskName.c_str());
-        }
-
-        pendingTwoPhaseTasks_.push_back({ type, task, nullptr, taskName });
     }
+    
+    if (taskName.compare("volume") == 0) {
+        MEDIA_LOGD("0x%{public}06" PRIXPTR " task[%{public}s] is in processing, the new task[%{public}s]",
+            FAKE_POINTER(this), currTwoPhaseTaskName_.c_str(), taskName.c_str());
+    } else {
+        MEDIA_LOGI("0x%{public}06" PRIXPTR " task[%{public}s] is in processing, the new task[%{public}s]",
+            FAKE_POINTER(this), currTwoPhaseTaskName_.c_str(), taskName.c_str());
+    }
+    
+    pendingTwoPhaseTasks_.push_back({ type, task, nullptr, taskName });
     return MSERR_OK;
 }
 
