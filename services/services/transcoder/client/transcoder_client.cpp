c5ebbcb19c2027de8eff5fe46571b97a51e50c60
任军旗 <renjunqi3@huawei.com>
2024-07-30 09:09:21 +0000

    update services/services/transcoder/client/transcoder_client.cpp. 修改代码
    Signed-off-by: 任军旗 <renjunqi3@huawei.com>

--- services/services/transcoder/client/transcoder_client.cpp
+++ services/services/transcoder/client/transcoder_client.cpp
@@ -47,7 +47,7 @@ TransCoderClient::~TransCoderClient()
     {
         std::lock_guard<std::mutex> lock(mutex_);
         (void)DisableMonitor();
-        CHECK_AND_RETURN_LOG(transCoderProxy_ == nullptr,
+        CHECK_AND_RETURN_LOG(transCoderProxy_ != nullptr,
             "0x%{public}06" PRIXPTR " Instances destroy", FAKE_POINTER(this));
         (void)transCoderProxy_->DestroyStub();
         transCoderProxy_ = nullptr;
