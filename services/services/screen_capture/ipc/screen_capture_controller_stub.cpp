3a8fb91864e62b4e2283bdd172cbc87f0ed52509
liyang <liyang782@huawei.com>
2024-08-01 22:14:48 +0800

    修复不安全调用场景
    Signed-off-by: liyang <liyang782@huawei.com>

--- services/services/screen_capture/ipc/screen_capture_controller_stub.cpp
+++ services/services/screen_capture/ipc/screen_capture_controller_stub.cpp
@@ -26,6 +26,8 @@ constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, LOG_DOMAIN_SCREENCAPTUR
 namespace OHOS {
 namespace Media {
 
+static const std::string SCREEN_RECORDER_BUNDLE_NAME = "com.ohos.screenrecorder";
+
 sptr<ScreenCaptureControllerStub> ScreenCaptureControllerStub::Create()
 {
     MEDIA_LOGI("ScreenCaptureControllerStub::Create() start");
@@ -107,6 +109,12 @@ int32_t ScreenCaptureControllerStub::ReportAVScreenCaptureUserChoice(int32_t ses
     MEDIA_LOGI("ScreenCaptureControllerStub::ReportAVScreenCaptureUserChoice start 2");
     CHECK_AND_RETURN_RET_LOG(screenCaptureControllerServer_ != nullptr, false,
         "screen capture controller server is nullptr");
+    int32_t appUid = IPCSkeleton::GetCallingUid();
+    std::string appName = GetClientBundleName(appUid);
+    if (SCREEN_RECORDER_BUNDLE_NAME.compare(appName) != 0) {
+        MEDIA_LOGE("ScreenCaptureControllerStub called by app name %{public}s", appName.c_str());
+        return MSERR_INVALID_OPERATION;
+    }
     return screenCaptureControllerServer_->ReportAVScreenCaptureUserChoice(sessionId, choice);
 }
 
