9a8e375859d50b2b750cfd7c3777b3ba6106c497
liyikang <liyikang@h-partners.com>
2024-08-03 18:33:36 +0800

    逻辑修正
    Signed-off-by: liyikang <liyikang@h-partners.com>

--- frameworks/js/mediasource/media_source_napi.cpp
+++ frameworks/js/mediasource/media_source_napi.cpp
@@ -128,7 +128,7 @@ napi_value MediaSourceNapi::JsCreateMediaSourceWithUrl(napi_env env, napi_callba
     napi_new_instance(env, constructor, 0, nullptr, &jsMediaSource);
 
     std::shared_ptr<AVMediaSourceTmp> mediaSource = GetMediaSource(env, jsMediaSource);
-    if (mediaSource != nullptr) {
+    if (mediaSource == nullptr) {
         MEDIA_LOGE("JsCreateMediaSourceWithUrl GetMediaSource fail");
         return nullptr;
     }
