2f60efea28a781154079237074db8fd1a6a7e18e
fanpingan <fanpingan1@huawei.com>
2024-08-05 06:36:31 +0000

    update services/engine/histreamer/player/hiplayer_impl.h.
    Signed-off-by: fanpingan <fanpingan1@huawei.com>

--- services/engine/histreamer/player/hiplayer_impl.h
+++ services/engine/histreamer/player/hiplayer_impl.h
@@ -199,7 +199,7 @@ private:
     bool IsFileUrl(const std::string &url) const;
     bool IsValidPlayRange(int64_t start, int64_t end) const;
     int32_t GetRealPath(const std::string &url, std::string &realUrlPath) const;
-    void SetDefaultAudioRenderInfo();
+    void SetDefaultAudioRenderInfo(const std::vector<std::shared_ptr<Meta>> &trackInfos);
     void AppendPlayerMediaInfo();
     int64_t GetCurrentMillisecond();
     void UpdatePlayStatistics();
