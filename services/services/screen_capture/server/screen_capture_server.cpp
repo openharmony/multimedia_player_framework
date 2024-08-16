22e9650683a8367bf6e6c57452ecb66a6d0ccfa2
xuzhangchi <xuzhangchi@huawei.com>
2024-08-16 16:40:46 +0800

    录屏框架麦克风设置失败问题解决
    Signed-off-by: xuzhangchi <xuzhangchi@huawei.com>

--- services/services/screen_capture/server/screen_capture_server.cpp
+++ services/services/screen_capture/server/screen_capture_server.cpp
@@ -2080,7 +2080,8 @@ int32_t ScreenCaptureServer::OnVoIPStatusChanged(bool isInVoIPCall)
         ret = innerAudioCapture_->Resume();
         CHECK_AND_RETURN_RET_LOG(ret == MSERR_OK, ret, "innerAudioCapture Resume failed");
     }
-    if (!isInVoIPCall && innerAudioCapture_->GetAudioCapturerState() == CAPTURER_RECORDING && isMicrophoneOn_ &&
+    if (!isInVoIPCall && innerAudioCapture_->GetAudioCapturerState() == CAPTURER_RECORDING &&
+        micAudioCapture_ && micAudioCapture_->GetAudioCapturerState() == CAPTURER_RECORDING
         audioSource_ && audioSource_->GetSpeakerAliveStatus()) {
         ret = innerAudioCapture_->Pause();
         CHECK_AND_RETURN_RET_LOG(ret == MSERR_OK, ret, "innerAudioCapture Pause failed");
