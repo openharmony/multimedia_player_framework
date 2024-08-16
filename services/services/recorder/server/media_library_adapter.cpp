afac09ec271d44f0bee6f6004f1b895b072eeaa6
laamy <liuao25@huawei.com>
2024-08-16 11:08:04 +0800

    fix code
    Signed-off-by: laamy <liuao25@huawei.com>

--- services/services/recorder/server/media_library_adapter.cpp
+++ services/services/recorder/server/media_library_adapter.cpp
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <iomanip>
 #include <ctime>
 #include "media_library_adapter.h"
 #include "media_library_manager.h"
