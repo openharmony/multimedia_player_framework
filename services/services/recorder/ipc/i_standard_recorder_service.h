7fcde2454c8e5beeb4e6b405fdfc756b793ee48d
laamy <liuao25@huawei.com>
2024-08-24 11:36:51 +0000

    recordder support auto create
    Signed-off-by: laamy <liuao25@huawei.com>

--- services/services/recorder/ipc/i_standard_recorder_service.h
+++ services/services/recorder/ipc/i_standard_recorder_service.h
@@ -57,6 +57,7 @@ public:
     virtual int32_t SetMaxDuration(int32_t duration) = 0;
     virtual int32_t SetOutputFormat(OutputFormatType format) = 0;
     virtual int32_t SetOutputFile(int32_t fd) = 0;
+    virtual int32_t SetFileGenerationMode(FileGenerationMode mode) = 0;
     virtual int32_t SetNextOutputFile(int32_t fd)
     {
         (void)fd;
@@ -114,6 +115,7 @@ public:
         SET_MAX_DURATION,
         SET_OUTPUT_FORMAT,
         SET_OUTPUT_FILE,
+        SET_FILE_GENERATION_MODE,
         SET_NEXT_OUTPUT_FILE,
         SET_MAX_FILE_SIZE,
         SET_LOCATION,
