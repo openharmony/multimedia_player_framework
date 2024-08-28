92861303d92371799d7d45f3770bd9190f24361d
liyu233 <liyu287@huawei.com>
2024-08-28 22:10:57 +0800

    realpath告警修改
    Signed-off-by: liyu233 <liyu287@huawei.com>

--- services/engine/histreamer/player/dragging_player_agent.cpp
+++ services/engine/histreamer/player/dragging_player_agent.cpp
@@ -174,9 +174,9 @@ void DraggingPlayerAgent::Release()
  
 void *DraggingPlayerAgent::LoadLibrary()
 {
-    char path[PATH_MAX + 1] = {0x00};
+    char path[PATH_MAX] = {0x00};
     const char *inputPath = REFENCE_LIB_ABSOLUTE_PATH.c_str();
-    if ((strlen(inputPath) > PATH_MAX) || (realpath(inputPath, path) == nullptr)) {
+    if (strlen(inputPath > PATH_MAX) || realpath(inputPath, path) == nullptr) {
         MEDIA_LOG_E("dlopen failed due to Invalid path");
         return nullptr;
     }
