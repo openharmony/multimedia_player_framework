1d47654ca41e7d24a15cbe07c3267ef3ce2bda7f
fanpingan <fanpingan1@huawei.com>
2024-09-25 12:31:51 +0000

    update services/services/player/ipc/player_service_stub.cpp.
    Signed-off-by: fanpingan <fanpingan1@huawei.com>

--- services/services/player/ipc/player_service_stub.cpp
+++ services/services/player/ipc/player_service_stub.cpp
@@ -177,8 +177,8 @@ void PlayerServiceStub::FillPlayerFuncPart2()
         [this](MessageParcel &data, MessageParcel &reply) { return SetMaxAmplitudeCbStatus(data, reply); } };
     playerFuncs_[SET_DEVICE_CHANGE_CB_STATUS] = { "Player::SetDeviceChangeCbStatus",
         [this](MessageParcel &data, MessageParcel &reply) { return SetDeviceChangeCbStatus(data, reply); } };
-    playerFuncs_[GET_LIVES_CURRENT_TIME] = { "GetLiveStreamCurrentTime",
-        [this](MessageParcel &data, MessageParcel &reply) { return GetLiveStreamCurrentTime(data, reply); } };
+    playerFuncs_[GET_LIVES_CURRENT_TIME] = { "GetPlaybackPosition",
+        [this](MessageParcel &data, MessageParcel &reply) { return GetPlaybackPosition(data, reply); } };
 }
 
 int32_t PlayerServiceStub::Init()
@@ -394,11 +394,11 @@ int32_t PlayerServiceStub::GetCurrentTime(int32_t &currentTime)
     return playerServer_->GetCurrentTime(currentTime);
 }
 
-int32_t PlayerServiceStub::GetLiveStreamCurrentTime(int32_t &currentTime)
+int32_t PlayerServiceStub::GetPlaybackPosition(int32_t &currentTime)
 {
-    MediaTrace trace("PlayerServiceStub::GetLiveStreamCurrentTime");
+    MediaTrace trace("PlayerServiceStub::GetPlaybackPosition");
     CHECK_AND_RETURN_RET_LOG(playerServer_ != nullptr, MSERR_NO_MEMORY, "player server is nullptr");
-    return playerServer_->GetLiveStreamCurrentTime(currentTime);
+    return playerServer_->GetPlaybackPosition(currentTime);
 }
 
 int32_t PlayerServiceStub::GetVideoTrackInfo(std::vector<Format> &videoTrack)
@@ -763,11 +763,11 @@ int32_t PlayerServiceStub::GetCurrentTime(MessageParcel &data, MessageParcel &re
     return MSERR_OK;
 }
 
-int32_t PlayerServiceStub::GetLiveStreamCurrentTime(MessageParcel &data, MessageParcel &reply)
+int32_t PlayerServiceStub::GetPlaybackPosition(MessageParcel &data, MessageParcel &reply)
 {
     (void)data;
     int32_t currentTime = -1;
-    int32_t ret = GetLiveStreamCurrentTime(currentTime);
+    int32_t ret = GetPlaybackPosition(currentTime);
     reply.WriteInt32(currentTime);
     reply.WriteInt32(ret);
     return MSERR_OK;
