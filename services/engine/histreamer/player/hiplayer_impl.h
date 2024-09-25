4f2c8751dc757a051f268897fcef8a60671f3247
fanpingan <fanpingan1@huawei.com>
2024-09-25 11:47:16 +0000

    update services/engine/histreamer/player/hiplayer_impl.h.
    Signed-off-by: fanpingan <fanpingan1@huawei.com>

--- services/engine/histreamer/player/hiplayer_impl.h
+++ services/engine/histreamer/player/hiplayer_impl.h
@@ -118,8 +118,8 @@ public:
     int32_t SetParameter(const Format& params) override;
     int32_t SetObs(const std::weak_ptr<IPlayerEngineObs>& obs) override;
     int32_t GetCurrentTime(int32_t& currentPositionMs) override;
+    int32_t GetLiveStreamCurrentTime(int32_t& durationMs) override;
     int32_t GetDuration(int32_t& durationMs) override;
-    int32_t GetLiveStreamDuration(int32_t& durationMs) override;
     int32_t SetPlaybackSpeed(PlaybackRateMode mode) override;
     int32_t SetMediaSource(const std::shared_ptr<AVMediaSource> &mediaSource, AVPlayStrategy strategy) override;
     int32_t GetPlaybackSpeed(PlaybackRateMode& mode) override;
