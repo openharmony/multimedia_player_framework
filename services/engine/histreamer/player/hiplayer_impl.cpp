a62c1285be6cec473799fbafb0436aa5cb01e941
liyu233 <liyu287@huawei.com>
2024-09-25 18:01:09 +0800

    修复拖动播放离手SEEK时pipeline未暂停问题
    Signed-off-by: liyu233 <liyu287@huawei.com>

--- services/engine/histreamer/player/hiplayer_impl.cpp
+++ services/engine/histreamer/player/hiplayer_impl.cpp
@@ -2869,6 +2869,7 @@ int32_t HiPlayerImpl::ExitSeekContinous(bool align, int64_t seekContinousBatchNo
         draggingPlayerAgent_->Release();
         draggingPlayerAgent_ = nullptr;
     }
+    demuxer_->PauseDragging();
     if (align) {
         Seek(lastSeekContinousPos_, PlayerSeekMode::SEEK_CLOSEST, false);
     }
