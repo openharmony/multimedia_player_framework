e6c4df8b8bf4459845bc33057dd16a1f9802da8c
wangyan <wangyan431@huawei.com>
2024-10-23 10:20:20 +0800

    解决稳定性问题
    Signed-off-by: wangyan <wangyan431@huawei.com>

--- services/services/player/server/player_server.cpp
+++ services/services/player/server/player_server.cpp
@@ -1244,6 +1244,9 @@ int32_t PlayerServer::SetVideoSurface(sptr<Surface> surface)
         std::lock_guard<std::mutex> surfaceLock(surfaceMutex_);
         surface_ = surface;
     }
+    CHECK_AND_RETURN_RET_LOG(!switchSurface || playerEngine_ == nullptr, MSERR_OK,
+        "current state: %{public}s, playerEngine == nullptr : %{public}d, can not SetVideoSurface",
+        GetStatusDescription(lastOpStatus_).c_str(), playerEngine_ == nullptr);
     if (switchSurface && playerEngine_ != nullptr) {
         return MSERR_OK;
     }
