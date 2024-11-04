311a4826c2b68868f722b0d73d106297200085f9
zhangqiang <zhangqiang342@huawei.com>
2024-11-04 09:51:34 +0800

    20241104   SoundPool增加ThreadPool来停止和音频的回调，防止和音频死锁
    Signed-off-by: zhangqiang <zhangqiang342@huawei.com>

--- frameworks/native/soundpool/cache_buffer.h
+++ frameworks/native/soundpool/cache_buffer.h
@@ -24,6 +24,7 @@
 #include "media_description.h"
 #include "cpp/mutex.h"
 #include "media_dfx.h"
+#include "thread_pool.h"
 #include "audio_system_manager.h"
 
 namespace OHOS {
@@ -51,7 +52,8 @@ public:
     CacheBuffer(const Format &trackFormat,
         const std::deque<std::shared_ptr<AudioBufferEntry>> &cacheData,
         const size_t &cacheDataTotalSize,
-        const int32_t &soundID, const int32_t &streamID);
+        const int32_t &soundID, const int32_t &streamID,
+        std::shared_ptr<ThreadPool> cacheBufferStopThreadPool);
     ~CacheBuffer();
     void OnWriteData(size_t length) override;
     void OnFirstFrameWriting(uint64_t latency) override;
@@ -112,6 +114,7 @@ private:
     std::shared_ptr<ISoundPoolCallback> cacheBufferCallback_ = nullptr;
     std::shared_ptr<ISoundPoolFrameWriteCallback> frameWriteCallback_ = nullptr;
     ffrt::mutex cacheBufferLock_;
+    std::weak_ptr<ThreadPool> cacheBufferStopThreadPool_;
 
     int32_t loop_ = 0;
     int32_t priority_ = 0;
