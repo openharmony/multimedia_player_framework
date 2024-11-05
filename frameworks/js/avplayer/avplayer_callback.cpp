69036ce2eacc373ecccd5fa6d98ba57c79b68497
Feng Lin <linfeng67@huawei.com>
2024-11-05 13:09:22 +0000

    ad
    Signed-off-by: Feng Lin <linfeng67@huawei.com>

--- frameworks/js/avplayer/avplayer_callback.cpp
+++ frameworks/js/avplayer/avplayer_callback.cpp
@@ -756,7 +756,6 @@ void AVPlayerCallback::OnError(int32_t errorCode, const std::string &errorMsg)
         getApiVersionFlag_ = false;
     }
     MediaServiceExtErrCodeAPI9 errorCodeApi9 = MSErrorToExtErrorAPI9(static_cast<MediaServiceErrCode>(errorCode));
-    MediaServiceExtErrCodeAPI9 errorCodeApi9 = MSErrorToExtErrorAPI9(static_cast<MediaServiceErrCode>(errorcode));
     if (errorCodeApi9 == MSERR_EXT_API9_NO_PERMISSION ||
         errorCodeApi9 == MSERR_EXT_API9_NO_MEMORY ||
         errorCodeApi9 == MSERR_EXT_API9_TIMEOUT ||
