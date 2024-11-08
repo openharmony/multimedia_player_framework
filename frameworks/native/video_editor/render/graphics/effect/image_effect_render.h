a3b3b5d4b0e414831aff1dbfa8010ae333595eb2
易娱月 <yiyuyue@huawei.com>
2024-11-08 10:46:59 +0800

    修复算法输入错误
    Signed-off-by: 易娱月 <yiyuyue@huawei.com>

--- frameworks/native/video_editor/render/graphics/effect/image_effect_render.h
+++ frameworks/native/video_editor/render/graphics/effect/image_effect_render.h
@@ -16,7 +16,9 @@
 #ifndef OH_VEF_GRAPHICS_IMAGE_EFFECT_RENDER_H
 #define OH_VEF_GRAPHICS_IMAGE_EFFECT_RENDER_H
 
+#ifdef IMAGE_EFFECT_SUPPORT
 #include "image_effect.h"
+#endif
 #include "render/graphics/base/render_texture.h"
 #include "video_editor.h"
 
@@ -37,6 +39,7 @@ public:
 
 private:
     VEFError InitEffectFilter();
+    void SetImageValue(const int32_t value, const char valueName[]);
 
 private:
 #ifdef IMAGE_EFFECT_SUPPORT
