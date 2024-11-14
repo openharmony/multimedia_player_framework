88e11709c6523a299081e21f8dec59388a9f678e
Elysia灬 <huangtianyu15@huawei.com>
2024-11-14 01:53:49 +0000

    update services/engine/histreamer/player/hiplayer_impl.cpp.
    Signed-off-by: Elysia灬 <huangtianyu15@huawei.com>

--- services/engine/histreamer/player/hiplayer_impl.cpp
+++ services/engine/histreamer/player/hiplayer_impl.cpp
@@ -1065,7 +1065,7 @@ void HiPlayerImpl::UpdateMaxSeekLatency(PlayerSeekMode mode, int64_t seekStartTi
 void HiPlayerImpl::NotifySeek(Status rtv, bool flag, int64_t seekPos)
 {
     if (rtv != Status::OK) {
-        MEDIA_LOG_E("Seek done, seek error");
+        MEDIA_LOG_E_SHORT("Seek done, seek error");
         // change player state to PLAYER_STATE_ERROR when seek error.
         UpdateStateNoLock(PlayerStates::PLAYER_STATE_ERROR);
         Format format;
