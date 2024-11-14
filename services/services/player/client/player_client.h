296228a48cf24295209ed7ac3b898d1bfc9c99cb
fanpingan <fanpingan1@huawei.com>
2024-11-14 09:11:33 +0000

    update services/services/player/client/player_client.h.
    Signed-off-by: fanpingan <fanpingan1@huawei.com>

--- services/services/player/client/player_client.h
+++ services/services/player/client/player_client.h
@@ -49,7 +49,6 @@ public:
     int32_t ReleaseSync() override;
     int32_t SetVolume(float leftVolume, float rightVolume) override;
     int32_t GetCurrentTime(int32_t &currentTime) override;
-    int32_t GetPlaybackPosition(int32_t &currentTime) override;
     int32_t Seek(int32_t mSeconds, PlayerSeekMode mode) override;
     int32_t GetSubtitleTrackInfo(std::vector<Format> &subtitleTrack) override;
     int32_t GetPlaybackSpeed(PlaybackRateMode &mode) override;
