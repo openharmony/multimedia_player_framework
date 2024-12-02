e9fb73e0ffd32ccc6d3192a3690be9baa0d07b44
Steven <duanzhengteng@huawei.com>
2024-12-02 14:25:20 +0000

    update test/unittest/recorder_test/capi/include/recorder_unit_test.h.
    Signed-off-by: Steven <duanzhengteng@huawei.com>

--- test/unittest/recorder_test/capi/include/recorder_unit_test.h
+++ test/unittest/recorder_test/capi/include/recorder_unit_test.h
@@ -12,3 +12,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#ifndef RECORDER_UNIT_TEST_H
+#define RECORDER_UNIT_TEST_H
+
+#include "gtest/gtest.h"
+#include "recorder.h"
+#include "avrecorder.h"
+
+class NativeRecorderUnitTest : public testing::Test {
+public:
+    // SetUpTestCase: Called before all test cases
+    static void SetUpTestCase(void);
+    // TearDownTestCase: Called after all test case
+    static void TearDownTestCase(void);
+    // SetUp: Called before each test cases
+    void SetUp(void);
+    // TearDown: Called after each test cases
+    void TearDown(void);
+
+protected:
+    std::shared_ptr<OHOS::Media::Recorder> recorder_{ nullptr };
+};
+
+#endif // RECORDER_UNIT_TEST_H
