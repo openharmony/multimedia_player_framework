2db470249415f29e7156123f9a4276f8c5aade9b
wanglingyi <wanglingyi4@huawei.com>
2024-12-02 20:56:35 +0800

    1202-3
    Signed-off-by: wanglingyi <wanglingyi4@huawei.com>

--- services/engine/histreamer/avmetadatahelper/av_thumbnail_generator.cpp
+++ services/engine/histreamer/avmetadatahelper/av_thumbnail_generator.cpp
@@ -147,15 +147,15 @@ std::shared_ptr<Meta> AVThumbnailGenerator::GetVideoTrackInfo()
         if (trackMime_.find("video/") == 0) {
             Plugins::MediaType mediaType;
             CHECK_AND_RETURN_RET_LOG(trackInfos[index]->GetData(Tag::MEDIA_TYPE, mediaType), nullptr,
-                                     "GetTargetTrackInfo failed to get mediaType, index:%{public}d", index);
+                                     "GetTargetTrackInfo failed to get mediaType, index:%{public}zu", index);
             CHECK_AND_RETURN_RET_LOG(
                 mediaType == Plugins::MediaType::VIDEO, nullptr,
-                "GetTargetTrackInfo mediaType is not video, index:%{public}d, mediaType:%{public}d", index,
+                "GetTargetTrackInfo mediaType is not video, index:%{public}zu, mediaType:%{public}d", index,
                 static_cast<int32_t>(mediaType));
             CHECK_AND_RETURN_RET_LOG(trackInfos[index]->Get<Tag::VIDEO_FRAME_RATE>(frameRate_) && frameRate_ > 0,
                 nullptr, "failed to get video frame rate");
             trackIndex_ = index;
-            MEDIA_LOGI("0x%{public}06" PRIXPTR " GetTrackInfo success trackIndex_:%{public}d, trackMime_:%{public}s",
+            MEDIA_LOGI("0x%{public}06" PRIXPTR " GetTrackInfo success trackIndex_:%{public}zu, trackMime_:%{public}s",
                        FAKE_POINTER(this), trackIndex_, trackMime_.c_str());
             if (trackInfos[index]->Get<Tag::VIDEO_ROTATION>(rotation_)) {
                 MEDIA_LOGI("rotation %{public}d", static_cast<int32_t>(rotation_));
@@ -195,7 +195,7 @@ void AVThumbnailGenerator::OnInputBufferAvailable(uint32_t index, std::shared_pt
 
 void AVThumbnailGenerator::OnOutputBufferAvailable(uint32_t index, std::shared_ptr<AVBuffer> buffer)
 {
-    MEDIA_LOGD("OnOutputBufferAvailable index:%{public}u , pts %{public}ld", index, buffer->pts_);
+    MEDIA_LOGD("OnOutputBufferAvailable index:%{public}u , pts %{public}" PRId64, index, buffer->pts_);
     CHECK_AND_RETURN_LOG(videoDecoder_ != nullptr, "Video decoder not exist");
     bool isValidBuffer = buffer != nullptr && buffer->memory_ != nullptr &&
          (buffer->memory_->GetSize() != 0 || buffer->memory_->GetSurfaceBuffer() != nullptr);
@@ -230,7 +230,7 @@ void AVThumbnailGenerator::OnOutputBufferAvailable(uint32_t index, std::shared_p
             bufferIndex_ = index;
             avBuffer_ = buffer;
         }
-        MEDIA_LOGI("dstTime %{public}ld resTime %{public}ld", seekTime_, buffer->pts_);
+        MEDIA_LOGI("dstTime %{public}" PRId64 " resTime %{public}" PRId64, seekTime_, buffer->pts_);
         cond_.notify_all();
         PauseFetchFrame();
         return;
