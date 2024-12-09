b0bb76939903a4396d95f46357233b41e0c9f533
彭同辉 <pengtonghui@huawei.com>
2024-12-09 13:00:26 +0000

    update services/services/avmetadatahelper/server/avmetadatahelper_server.h.
    Signed-off-by: 彭同辉 <pengtonghui@huawei.com>

--- services/services/avmetadatahelper/server/avmetadatahelper_server.h
+++ services/services/avmetadatahelper/server/avmetadatahelper_server.h
@@ -58,7 +58,7 @@ private:
     int32_t appUid_;
     std::shared_ptr<IAVMetadataHelperEngine> avMetadataHelperEngine_ = nullptr;
     std::mutex mutex_;
-    std::condition_variable cond_;
+    std::condition_variable ipcReturnCond_;
     std::unique_ptr<UriHelper> uriHelper_;
     TaskQueue taskQue_;
     std::shared_ptr<IMediaDataSource> dataSrc_ = nullptr;
