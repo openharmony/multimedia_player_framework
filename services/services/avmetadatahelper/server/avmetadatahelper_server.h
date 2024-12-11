2f653dff8092e911a0492ac402ea427fe70f601c
彭同辉 <pengtonghui@huawei.com>
2024-12-11 07:05:12 +0000

    update services/services/avmetadatahelper/server/avmetadatahelper_server.h.
    Signed-off-by: 彭同辉 <pengtonghui@huawei.com>

--- services/services/avmetadatahelper/server/avmetadatahelper_server.h
+++ services/services/avmetadatahelper/server/avmetadatahelper_server.h
@@ -54,8 +54,7 @@ private:
     void NotifyErrorCallback(int32_t code, const std::string msg);
     void NotifyInfoCallback(HelperOnInfoType type, int32_t extra);
     int32_t InitEngine(const std::string &uri);
-    int32_t CheckSourceByUri(const std::string &uri);
-    int32_t CheckSourceByFd(int32_t fd, int64_t offset, int64_t size);
+    int32_t CheckSourceByUriHelper();
 
     int32_t appUid_;
     std::shared_ptr<IAVMetadataHelperEngine> avMetadataHelperEngine_ = nullptr;
