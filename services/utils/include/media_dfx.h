e825d512fc59a56fed569e5b180088749fe7cab3
wangyan <wangyan431@huawei.com>
2024-12-17 06:36:37 +0000

    update services/utils/include/media_dfx.h.
    Signed-off-by: wangyan <wangyan431@huawei.com>

--- services/utils/include/media_dfx.h
+++ services/utils/include/media_dfx.h
@@ -25,7 +25,7 @@
 #include "nocopyable.h"
 #include "hisysevent.h"
 #include "meta/meta.h"
-#ifdef CROSS_PLATFORM
+#ifndef CROSS_PLATFORM
 #include "nlohmann/json.hpp"
 #endif
 #include <chrono>
@@ -33,7 +33,7 @@
 
 namespace OHOS {
 namespace Media {
-#ifdef CROSS_PLATFORM
+#ifndef CROSS_PLATFORM
 using json = nlohmann::json;
 #endif
 enum CallType {
@@ -73,7 +73,7 @@ public:
 private:
     void StatisicsHiSysEventWrite(CallType callType, OHOS::HiviewDFX::HiSysEvent::EventType type,
         const std::vector<std::string>& infoArr);
-#ifdef CROSS_PLATFORM
+#ifndef CROSS_PLATFORM
     void ParseOneEvent(const std::pair<uint64_t, std::shared_ptr<OHOS::Media::Meta>> &listPair, json& metaInfoJson);
 #endif
     std::string msg_;
