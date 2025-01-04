bd1829a9ee742352462c69901a46aec455ae0fe7
Trouvaille <wuminghua6@huawei.com>
2025-01-04 19:20:31 +0800

    录屏框架新增inner接口给系统SA传递appInfo信息
    Signed-off-by: Trouvaille <wuminghua6@huawei.com>

---
+++ test/fuzztest/screen_capture_fuzztest/screencapturesaappinfo_fuzzer/screencapturesaappinfo_fuzzer.cpp
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <cmath>
+#include <iostream>
+#include "aw_common.h"
+#include "string_ex.h"
+#include "media_errors.h"
+#include "directory_ex.h"
+#include "screen_capture.h"
+#include "screencapturesaappinfo_fuzzer.h"
+#include "ipc_skeleton.h"
+
+using namespace std;
+using namespace OHOS;
+using namespace Media;
+
+namespace OHOS {
+namespace Media {
+ScreenCaptureSaAppInfoFuzzer::ScreenCaptureSaAppInfoFuzzer()
+{
+}
+
+ScreenCaptureSaAppInfoFuzzer::~ScreenCaptureSaAppInfoFuzzer()
+{
+}
+
+void SetConfig(AVScreenCaptureConfig &config)
+{
+    AudioCaptureInfo miccapinfo = {
+        .audioSampleRate = 48000,
+        .audioChannels = 2,
+        .audioSource = SOURCE_DEFAULT
+    };
+
+    VideoCaptureInfo videocapinfo = {
+        .videoFrameWidth = 720,
+        .videoFrameHeight = 1280,
+        .videoSource = VIDEO_SOURCE_SURFACE_RGBA
+    };
+
+    AudioInfo audioinfo = {
+        .micCapInfo = miccapinfo,
+    };
+
+    VideoInfo videoinfo = {
+        .videoCapInfo = videocapinfo
+    };
+
+    config = {
+        .captureMode = CAPTURE_HOME_SCREEN,
+        .dataType = ORIGINAL_STREAM,
+        .audioInfo = audioinfo,
+        .videoInfo = videoinfo,
+    };
+}
+
+bool ScreenCaptureSaAppInfoFuzzer::FuzzScreenCaptureSaAppInfo(uint8_t *data, size_t size)
+{
+    if (data == nullptr || size < sizeof(OHOS::AudioStandard::AppInfo)) {
+        return false;
+    }
+
+    OHOS::AudioStandard::AppInfo appInfo;
+    appInfo.appTokenId = IPCSkeleton::GetCallingTokenID();
+    appInfo.appFullTokenId = IPCSkeleton::GetCallingFullTokenID();
+    appInfo.appUid = IPCSkeleton::GetCallingUid();
+    appInfo.appPid = IPCSkeleton::GetCallingPid();
+
+    bool retFlags = TestScreenCapture::CreateScreenCapture(appInfo);
+    RETURN_IF(retFlags, false);
+
+    AVScreenCaptureConfig config;
+    SetConfig(config);
+    TestScreenCapture::Init(config);
+    TestScreenCapture::StartScreenCapture();
+    TestScreenCapture::StopScreenCapture();
+    TestScreenCapture::Release();
+    return true;
+}
+} // namespace Media
+
+bool FuzzTestScreenCaptureSaAppInfo(uint8_t *data, size_t size)
+{
+    if (data == nullptr) {
+        return true;
+    }
+
+    if (size < sizeof(OHOS::AudioStandard::AppInfo)) {
+        return true;
+    }
+
+    ScreenCaptureSaAppInfoFuzzer testScreenCapture;
+    return testScreenCapture.FuzzScreenCaptureSaAppInfo(data, size);
+}
+} // namespace OHOS
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    OHOS::FuzzTestScreenCaptureSaAppInfo(data, size);
+    return 0;
+}
