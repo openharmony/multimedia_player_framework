6d23b63cdc545340a9e51a096d43263715f80c99
Feng Lin <linfeng67@huawei.com>
2025-01-10 03:01:13 +0000

    ad
    Signed-off-by: Feng Lin <linfeng67@huawei.com>

--- services/services/player/server/player_server.cpp
+++ services/services/player/server/player_server.cpp
@@ -43,8 +43,8 @@ namespace {
     constexpr int32_t MAX_INT_DIGIT = 9;
     bool IsNumber(const std::string& str)
     {
-        bool g_isIntStr = str.length() > 0 && str.length() <= MAX_INT_DIGIT;
-        return str.find_first_not_of("0123456789") == std::string::npos && g_isIntStr;
+        bool isIntStr = str.length() > 0 && str.length() <= MAX_INT_DIGIT;
+        return str.find_first_not_of("0123456789") == std::string::npos && isIntStr;
     }
 }
 
