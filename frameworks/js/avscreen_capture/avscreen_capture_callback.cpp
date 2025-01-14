ccb1be589f20038e7090c10a3a3b8b72367bc0cb
xuzhangchi <xuzhangchi@huawei.com>
2025-01-14 20:57:17 +0800

    AVScreenCapture napi 异步调用整改
    Signed-off-by: xuzhangchi <xuzhangchi@huawei.com>

--- frameworks/js/avscreen_capture/avscreen_capture_callback.cpp
+++ frameworks/js/avscreen_capture/avscreen_capture_callback.cpp
@@ -145,7 +145,7 @@ void AVScreenCaptureCallback::OnJsErrorCallBack(AVScreenCaptureJsCallback *jsCb)
             CHECK_AND_BREAK_LOG(nstatus == napi_ok, "%{public}s fail to napi call function", request.c_str());
         } while (0);
         delete jsCb;
-    }
+    };
     CHECK_AND_RETURN_LOG(napi_send_event(env_, task, napi_eprio_immediate) == napi_status::napi_ok,
         "OnJsErrorCallBack napi_send_event failed");
 
