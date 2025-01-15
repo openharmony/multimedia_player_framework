3cc356912b77dbcda68064004557a94b335c5dec
xuzhangchi <xuzhangchi@huawei.com>
2025-01-15 17:27:13 +0800

    录屏框架 napi 异步调用整改
    Signed-off-by: xuzhangchi <xuzhangchi@huawei.com>

--- frameworks/js/avscreen_capture/avscreen_capture_napi.cpp
+++ frameworks/js/avscreen_capture/avscreen_capture_napi.cpp
@@ -160,14 +160,12 @@ napi_value AVScreenCaptureNapi::JsCreateAVScreenRecorder(napi_env env, napi_call
     asyncCtx->deferred = CommonNapi::CreatePromise(env, asyncCtx->callbackRef, result);
     asyncCtx->JsResult = std::make_unique<MediaJsResultInstance>(constructor_);
     asyncCtx->ctorFlag = true;
-
-    napi_value resource = nullptr;
-    napi_create_string_utf8(env, "JsCreateAVScreenRecorder", NAPI_AUTO_LENGTH, &resource);
-    NAPI_CALL(env, napi_create_async_work(env, nullptr, resource, [](napi_env env, void* data) {
-        MEDIA_LOGD("JsCreateAVScreenRecorder napi_create_async_work");
-    }, MediaAsyncContext::CompleteCallback, static_cast<void *>(asyncCtx.get()), &asyncCtx->work));
-    NAPI_CALL(env, napi_queue_async_work_with_qos(env, asyncCtx->work, napi_qos_user_initiated));
-    asyncCtx.release();
+    auto ret = MediaAsyncContext::SendCompleteEvent(env, asyncCtx.get(), napi_eprio_immediate);
+    if (ret != napi_status::napi_ok) {
+        MEDIA_LOGE("failed to SendEvent, ret = %{public}d", ret);
+    } else {
+        asyncCtx.release();
+    }
 
     MEDIA_LOGI("JsCreateAVScreenRecorder success");
     return result;
