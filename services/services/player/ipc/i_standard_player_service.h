a9aa0497f1f606f565e09062f17e84296d735c4c
xulei123 <xulei272@huawei.com>
2025-01-16 13:10:01 +0000

    sei
    Signed-off-by: xulei123 <xulei272@huawei.com>

--- services/services/player/ipc/i_standard_player_service.h
+++ services/services/player/ipc/i_standard_player_service.h
@@ -131,7 +131,11 @@ public:
         return false;
     }
 
-    virtual int32_t GetPlaybackPosition(int32_t &playbackPosition) = 0;
+    virtual int32_t GetPlaybackPosition(int32_t &playbackPosition)
+    {
+        (void)playbackPosition;
+        return 0;
+    }
 
     virtual int32_t SetSeiMessageCbStatus(bool status, const std::vector<int32_t> &payloadTypes)
     {
