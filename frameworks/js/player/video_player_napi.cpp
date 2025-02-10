8ccedabefdd5ec8a0da546631ce1f4171bbc5e2b
Feng Lin <linfeng67@huawei.com>
2025-02-10 12:20:33 +0000

    add verification for surface
    Signed-off-by: Feng Lin <linfeng67@huawei.com>

--- frameworks/js/player/video_player_napi.cpp
+++ frameworks/js/player/video_player_napi.cpp
@@ -27,6 +27,22 @@
 
 namespace {
     constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, LOG_DOMAIN_PLAYER, "VideoPlayerNapi"};
+    constexpr int32_t BASE = 10;
+    bool StrToULL(const std::string &str, uint64_t &value)
+    {
+        CHECK_AND_RETURN_RET(!str.empty() && (isdigit(str.front())), false);
+        std::string valStr(str);
+        const char* addr = valStr.c_str();
+        char* end = nullptr;
+        errno = 0;
+        unsigned long long result = strtoull(addr, &end, BASE);
+        CHECK_AND_RETURN_RET_LOG(result <= ULLONG_MAX, false,
+            "call StrToULL func false,  input str is: %{public}s!", valStr.c_str());
+        CHECK_AND_RETURN_RET_LOG(end != addr && end[0] == '\0' && errno != ERANGE, false,
+            "call StrToULL func false,  input str is: %{public}s!", valStr.c_str());
+        value = result;
+        return true;
+    }
 }
 
 namespace OHOS {
@@ -406,7 +422,11 @@ void VideoPlayerNapi::AsyncSetDisplaySurface(napi_env env, void *data)
         asyncContext->SignError(MSERR_EXT_INVALID_VAL, "input surface id is invalid");
         return;
     }
-    surfaceId = std::stoull(asyncContext->surface);
+    if (!StrToULL(asyncContext->surface, surfaceId)) {
+        OnErrorCb(MSERR_EXT_API9_INVALID_PARAMETER,
+            "invalid parameters, failed to obtain surfaceId");
+        return;
+    }
     MEDIA_LOGD("get surface, surfaceId = (%{public}" PRIu64 ")", surfaceId);
 
     auto surface = SurfaceUtils::GetInstance()->GetSurface(surfaceId);
