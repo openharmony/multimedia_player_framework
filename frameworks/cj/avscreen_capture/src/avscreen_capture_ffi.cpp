7805201e921292194faf9fbb7094639c9aaf9027
小城茶语 <guorunzhi@huawei.com>
2025-02-19 16:45:07 +0800

    fix screen capture recorder
    Signed-off-by: 小城茶语 <guorunzhi@huawei.com>

--- frameworks/cj/avscreen_capture/src/avscreen_capture_ffi.cpp
+++ frameworks/cj/avscreen_capture/src/avscreen_capture_ffi.cpp
@@ -47,29 +47,29 @@ extern "C" {
         fconfig.audioInfo.innerCapInfo.audioSampleRate = config.audioInfo.innerCapInfo.audioSampleRate;
         fconfig.audioInfo.micCapInfo.audioChannels = config.audioInfo.micCapInfo.audioChannels;
         fconfig.audioInfo.innerCapInfo.audioChannels = config.audioInfo.innerCapInfo.audioChannels;
-        fconfig.audioInfo.micCapInfo.audioSource = 
-            static_cast<AudioCaptureSourceType>(config.audioInfo.micCapInfo.audioSource);
-        fconfig.audioInfo.innerCapInfo.audioSource = 
-            static_cast<AudioCaptureSourceType>(config.audioInfo.innerCapInfo.audioSource);
+        fconfig.audioInfo.micCapInfo.audioSource = static_cast<AudioCaptureSourceType>(
+            config.audioInfo.micCapInfo.audioSource);
+        fconfig.audioInfo.innerCapInfo.audioSource = static_cast<AudioCaptureSourceType>(
+            config.audioInfo.innerCapInfo.audioSource);
         fconfig.audioInfo.audioEncInfo.audioBitrate = config.audioInfo.audioEncInfo.audioBitrate;
-        fconfig.audioInfo.audioEncInfo.audioCodecformat = 
-            static_cast<AudioCodecFormat>(config.audioInfo.audioEncInfo.audioCodecformat);
+        fconfig.audioInfo.audioEncInfo.audioCodecformat = static_cast<AudioCodecFormat>(
+            config.audioInfo.audioEncInfo.audioCodecformat);
 
         int32_t displayId = 0;
         fconfig.videoInfo.videoCapInfo.displayId = static_cast<uint64_t>(displayId);
         fconfig.videoInfo.videoCapInfo.videoFrameHeight = config.videoInfo.videoCapInfo.videoFrameHeight;
         fconfig.videoInfo.videoCapInfo.videoFrameWidth = config.videoInfo.videoCapInfo.videoFrameWidth;
-        fconfig.videoInfo.videoCapInfo.videoSource = 
-            static_cast<VideoSourceType>(config.videoInfo.videoCapInfo.videoSource);
+        fconfig.videoInfo.videoCapInfo.videoSource = static_cast<VideoSourceType>(
+            config.videoInfo.videoCapInfo.videoSource);
 
-        fconfig.videoInfo.videoEncInfo.videoCodec = 
-            static_cast<VideoCodecFormat>(config.videoInfo.videoEncInfo.videoCodec);
+        fconfig.videoInfo.videoEncInfo.videoCodec = static_cast<VideoCodecFormat>(
+            config.videoInfo.videoEncInfo.videoCodec);
         fconfig.videoInfo.videoEncInfo.videoBitrate = config.videoInfo.videoEncInfo.videoBitrate;
-        int32_t frame_rate = 60;
-        fconfig.videoInfo.videoEncInfo.videoFrameRate = frame_rate;
+        int32_t frameRate = 60;
+        fconfig.videoInfo.videoEncInfo.videoFrameRate = frameRate;
 
-        std::string file_format = "mp4";
-        fconfig.recorderInfo.fileFormat = file_format;
+        std::string fileFormat = "mp4";
+        fconfig.recorderInfo.fileFormat = fileFormat;
         fconfig.recorderInfo.url = config.recorderInfo.url;
         
         return cjAVScreenCapture->AVScreenCaptureinit(screenCapture, fconfig);
