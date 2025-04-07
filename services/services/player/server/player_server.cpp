51cee34a0be3de6255757fa0e7768e02ba006860
hanwenzhao <hanwenzhao@huawei.com>
2025-04-07 10:33:21 +0800

    fix ut fail
    Signed-off-by: hanwenzhao <hanwenzhao@huawei.com>
    Change-Id: Ia8e6f81d75baa54eef3ef38d45a9e9ffe2fe11e8

--- services/services/player/server/player_server.cpp
+++ services/services/player/server/player_server.cpp
@@ -1930,6 +1930,7 @@ bool PlayerServer::IsBootCompleted()
 
 int32_t PlayerServer::SetMediaMuted(OHOS::Media::MediaType mediaType, bool isMuted)
 {
+    CHECK_AND_RETURN_RET(mediaType == OHOS::Media::MediaType::MEDIA_TYPE_AUD, MSERR_INVALID_VAL);
     std::lock_guard<std::mutex> lock(mutex_);
     CHECK_AND_RETURN_RET(lastOpStatus_ == PLAYER_INITIALIZED || lastOpStatus_ == PLAYER_PREPARED ||
                              lastOpStatus_ == PLAYER_STARTED || lastOpStatus_ == PLAYER_PLAYBACK_COMPLETE ||
