a19ac73e680eadab38c8bbf85b3b561ea4bc7587
李一鸣 <liyiming21@huawei.com>
2025-04-08 06:27:00 +0000

    update services/services/screen_capture/server/screen_capture_server.cpp.
    Signed-off-by: 李一鸣 <liyiming21@huawei.com>

--- services/services/screen_capture/server/screen_capture_server.cpp
+++ services/services/screen_capture/server/screen_capture_server.cpp
@@ -2841,7 +2841,7 @@ int32_t ScreenCaptureServer::StartMicAudioCapture()
     MEDIA_LOGI("ScreenCaptureServer: 0x%{public}06" PRIXPTR " StartMicAudioCapture start, dataType:%{public}d, "
         "micCapInfo.state:%{public}d.",
         FAKE_POINTER(this), captureConfig_.dataType, captureConfig_.audioInfo.micCapInfo.state);
-
+    CHECK_AND_RETURN_RET(micAudioCapture_ != nullptr, MSERR_OK);
     if (captureConfig_.audioInfo.micCapInfo.state == AVScreenCaptureParamValidationState::VALIDATION_VALID) {
         MediaTrace trace("ScreenCaptureServer::StartMicAudioCapture");
         if (audioSource_) {
