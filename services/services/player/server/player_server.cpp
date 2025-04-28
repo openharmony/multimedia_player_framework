be7d899b01d281aec1bf7e81ab8e198bdcfc9a55
韩宗明 <hanzongming@h-partners.com>
2025-04-28 04:00:13 +0000

    保留原来消息新增，而非替换
    Signed-off-by: 韩宗明 <hanzongming@h-partners.com>

--- services/services/player/server/player_server.cpp
+++ services/services/player/server/player_server.cpp
@@ -1576,7 +1576,8 @@ int32_t PlayerServer::DumpInfo(int32_t fd)
 void PlayerServer::OnError(PlayerErrorType errorType, int32_t errorCode)
 {
     (void)errorType;
-    auto errorMsg = MSErrorToString(static_cast<MediaServiceErrCode>(errorCode));
+    auto errorMsg = MSErrorToExtErrorString(static_cast<MediaServiceErrCode>(errorCode)) + ", ";
+    errorMsg += MSErrorToString(static_cast<MediaServiceErrCode>(errorCode));
     return OnErrorMessage(errorCode, errorMsg);
 }
 
