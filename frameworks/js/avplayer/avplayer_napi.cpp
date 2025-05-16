18cda2bbe076c29155a66d6b033086dcdad56431
邵俊杰 <shaojunjie5@huawei.com>
2025-05-16 02:55:11 +0000

    修复死锁
    Signed-off-by: 邵俊杰 <shaojunjie5@huawei.com>

--- frameworks/js/avplayer/avplayer_napi.cpp
+++ frameworks/js/avplayer/avplayer_napi.cpp
@@ -1155,7 +1155,7 @@ void AVPlayerNapi::EnqueueNetworkTask(const std::string url)
         std::unique_lock<std::mutex> lock(taskMutex_);
         auto state = GetCurrentState();
         if (state != AVPlayerState::STATE_IDLE) {
-            OnErrorCb(MSERR_EXT_API9_OPERATE_NOT_PERMIT, "current state is not idle, unsupport set url");
+            QueueOnErrorCb(MSERR_EXT_API9_OPERATE_NOT_PERMIT, "current state is not idle, unsupport set url");
             return;
         }
         if (player_ != nullptr) {
@@ -1177,7 +1177,7 @@ void AVPlayerNapi::EnqueueFdTask(const int32_t fd)
         std::unique_lock<std::mutex> lock(taskMutex_);
         auto state = GetCurrentState();
         if (state != AVPlayerState::STATE_IDLE) {
-            OnErrorCb(MSERR_EXT_API9_OPERATE_NOT_PERMIT, "current state is not idle, unsupport set source fd");
+            QueueOnErrorCb(MSERR_EXT_API9_OPERATE_NOT_PERMIT, "current state is not idle, unsupport set source fd");
             return;
         }
         if (player_ != nullptr) {
