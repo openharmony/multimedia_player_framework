ab2594de525f0baaed160101bd2abcad38b1d3a4
HannibalQSP <qianshenpeng@huawei.com>
2025-05-19 16:28:11 +0800

    fix
    Signed-off-by: HannibalQSP <qianshenpeng@huawei.com>

--- frameworks/native/recorder/recorder_impl.cpp
+++ frameworks/native/recorder/recorder_impl.cpp
@@ -311,16 +311,18 @@ int32_t RecorderImpl::Start()
 {
     MEDIA_LOGI("RecorderImpl:0x%{public}06" PRIXPTR " Start in", FAKE_POINTER(this));
     CHECK_AND_RETURN_RET_LOG(recorderService_ != nullptr, MSERR_INVALID_OPERATION, "recorder service does not exist..");
-    CHECK_AND_RETURN_RET_LOG(surface_ != nullptr, MSERR_INVALID_OPERATION, "surface is nullptr.");
-    std::string surfaceAppFwkType = surface_->GetSurfaceAppFrameworkType();
-    if (APP_USE_COMPATIBLE_MODE == surfaceAppFwkType) {
-        GraphicTransformType type = surface_->GetTransform();
-        if (type >= GraphicTransformType::GRAPHIC_ROTATE_NONE
-            && type <= GraphicTransformType::GRAPHIC_ROTATE_270) {
-            int32_t rotation = static_cast<int32_t>(type) * ROTATION_BASE;
-            MEDIA_LOGI("RecorderImpl auto set orientation is %{public}d", rotation);
-            SetOrientationHint(rotation);
-        }
+    if (surface_) {
+        MEDIA_LOGI("surface not nullptr");
+        std::string surfaceAppFwkType = surface_->GetSurfaceAppFrameworkType();
+        if (APP_USE_COMPATIBLE_MODE == surfaceAppFwkType) {
+            GraphicTransformType type = surface_->GetTransform();
+            if (type >= GraphicTransformType::GRAPHIC_ROTATE_NONE
+                && type <= GraphicTransformType::GRAPHIC_ROTATE_270) {
+                int32_t rotation = static_cast<int32_t>(type) * ROTATION_BASE;
+                MEDIA_LOGI("RecorderImpl auto set orientation is %{public}d", rotation);
+                SetOrientationHint(rotation);
+            }
+        }   
     }
     return recorderService_->Start();
 }
