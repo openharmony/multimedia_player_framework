0dd143321669b1f80ee6a20af557e299473d9caa
zhanglei <zhanglei737@huawei.com>
2025-06-05 23:31:33 -0700

    camera idl
    Signed-off-by: zhanglei <zhanglei737@huawei.com>

--- services/services/recorder/server/recorder_server.cpp
+++ services/services/recorder/server/recorder_server.cpp
@@ -28,7 +28,7 @@
 #include "media_library_adapter.h"
 #include "system_ability_definition.h"
 #include "iservice_registry.h"
-#include "hcamera_service_proxy.h"
+#include "camera_service_proxy.h"
 #endif
 #ifdef SUPPORT_POWER_MANAGER
 #include "shutdown/shutdown_priority.h"
@@ -1266,7 +1266,7 @@ bool RecorderServer::CheckCameraOutputState()
     CHECK_AND_RETURN_RET_LOG(samgr != nullptr, false, "Failed to get System ability manager");
     auto object = samgr->GetSystemAbility(CAMERA_SERVICE_ID);
     CHECK_AND_RETURN_RET_LOG(object != nullptr, false, "object is null");
-    sptr<CameraStandard::ICameraService> serviceProxy = iface_cast<CameraStandard::HCameraServiceProxy>(object);
+    sptr<CameraStandard::ICameraService> serviceProxy = iface_cast<CameraStandard::CameraServiceProxy>(object);
     CHECK_AND_RETURN_RET_LOG(serviceProxy != nullptr, false, "serviceProxy is null");
     int32_t status = 0;
     serviceProxy->GetCameraOutputStatus(IPCSkeleton::GetCallingPid(), status);
