897604f8d1c7e4a15cbbe2a579e516e491439c28
zhangwt3652 <zhangwenting15@huawei.com>
2025-07-07 12:23:14 +0800

    修复audio idl造成的内存泄漏
    Signed-off-by: zhangwt3652 <zhangwenting15@huawei.com>
    Change-Id: I91a1e3afdb40b68a62245ba76d0c836c3e2f2fdf

--- frameworks/js/avplayer/avplayer_callback.cpp
+++ frameworks/js/avplayer/avplayer_callback.cpp
@@ -751,7 +751,7 @@ void AVPlayerCallback::OnAudioDeviceChangeCb(const int32_t extra, const Format &
     Parcel parcel;
     parcel.WriteBuffer(parcelBuffer, parcelSize);
     AudioStandard::AudioDeviceDescriptor deviceInfo(AudioStandard::AudioDeviceDescriptor::DEVICE_INFO);
-    deviceInfo.Unmarshalling(parcel);
+    deviceInfo.UnmarshallingSelf(parcel);
 
     int32_t reason;
     infoBody.GetIntValue(PlayerKeys::AUDIO_DEVICE_CHANGE_REASON, reason);
