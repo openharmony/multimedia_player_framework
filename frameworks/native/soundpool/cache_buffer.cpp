66e5a2193d5c35a6a44fceca537aa4246609ee6d
liuwanlin9358 <liuwanlin2@h-partners.com>
2025-08-14 20:30:27 +0800

    SoundPool安全告警清理
    Signed-off-by: liuwanlin9358 <liuwanlin2@h-partners.com>

--- frameworks/native/soundpool/cache_buffer.cpp
+++ frameworks/native/soundpool/cache_buffer.cpp
@@ -398,12 +398,16 @@ void CacheBuffer::DealWriteData(size_t length)
             MEDIA_LOGI("DealWriteData ResetFirstFrameState when looping");
             audioRenderer_->ResetFirstFrameState();
         }
+    } else if (fullCacheData_ != nullptr) {
+        MEDIA_LOGE("OnWriteData, cacheDataFrameIndex_: %{public}zu, length: %{public}zu,"
+            " bufDesc.buffer:%{public}d, fullCacheData_:1, fullCacheData_->buffer:%{public}d,"
+            " streamID_:%{public}d",
+            cacheDataFrameIndex_, length, bufDesc.buffer != nullptr,
+            fullCacheData_->buffer != nullptr, streamID_);
     } else {
         MEDIA_LOGE("OnWriteData, cacheDataFrameIndex_: %{public}zu, length: %{public}zu,"
-            " bufDesc.buffer:%{public}d, fullCacheData_:%{public}d, fullCacheData_->buffer:%{public}d,"
-            " streamID_:%{public}d",
-            cacheDataFrameIndex_, length, bufDesc.buffer != nullptr, fullCacheData_ != nullptr,
-            fullCacheData_->buffer != nullptr, streamID_);
+            " bufDesc.buffer:%{public}d, fullCacheData_:0, streamID_:%{public}d",
+            cacheDataFrameIndex_, length, bufDesc.buffer != nullptr, streamID_);
     }
 }
 
