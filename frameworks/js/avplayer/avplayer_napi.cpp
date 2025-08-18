03e544c251aae84b7e083cb7098ff79610d33e7d
张万鹏 <zhangwanpeng5@huawei.com>
2025-08-18 11:31:45 +0000

    update frameworks/js/avplayer/avplayer_napi.cpp. 修改addSubtitleFromFd 方法中，offset与length非必填参数在代码中校验非空问题
    Signed-off-by: 张万鹏 <zhangwanpeng5@huawei.com>

--- frameworks/js/avplayer/avplayer_napi.cpp
+++ frameworks/js/avplayer/avplayer_napi.cpp
@@ -1187,18 +1187,14 @@ napi_value AVPlayerNapi::JsAddSubtitleAVFileDescriptor(napi_env env, napi_callba
     int32_t subtitleFd = -1;
     napi_status status = napi_get_value_int32(env, args[0], &subtitleFd);
     if (status != napi_ok) {
-        MEDIA_LOGE("JsAddSubtitleAVFileDescriptor status != napi_ok");
-        jsPlayer->OnErrorCb(MSERR_EXT_API9_INVALID_PARAMETER,
-            "invalid parameters, please check JsAddSubtitleAVFileDescriptor");
-        return result;
+        MEDIA_LOGI("JsAddSubtitleAVFileDescriptor status_offset != napi_ok");
+        offset = 0;
     }
     int64_t offset = -1;
     napi_status status_offset = napi_get_value_int64(env, args[1], &offset);
     if (status_offset != napi_ok) {
-        MEDIA_LOGE("JsAddSubtitleAVFileDescriptor status_offset != napi_ok");
-        jsPlayer->OnErrorCb(MSERR_EXT_API9_INVALID_PARAMETER,
-            "invalid parameters, please check JsAddSubtitleAVFileDescriptor");
-        return result;
+        MEDIA_LOGI("JsAddSubtitleAVFileDescriptor status_length != napi_ok");
+        length = 0;
     }
     int64_t length = -1;
     napi_status status_length = napi_get_value_int64(env, args[2], &length);
