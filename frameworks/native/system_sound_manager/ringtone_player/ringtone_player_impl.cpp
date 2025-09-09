c473daa320bdb4015c92049553be438b2337d07a
WangWanliang <wangwanliang1@h-partners.com>
2025-09-09 13:53:59 +0000

    update frameworks/native/system_sound_manager/ringtone_player/ringtone_player_impl.cpp.
    Signed-off-by: WangWanliang <wangwanliang1@h-partners.com>

--- frameworks/native/system_sound_manager/ringtone_player/ringtone_player_impl.cpp
+++ frameworks/native/system_sound_manager/ringtone_player/ringtone_player_impl.cpp
@@ -360,7 +360,7 @@ int32_t RingtonePlayerImpl::Configure(const float &volume, const bool &loop)
     std::lock_guard<std::mutex> lock(playerMutex_);
 
     if (configuredUri_ == NO_RING_SOUND && ringtoneState_ == STATE_RUNNING &&
-        std::abs(volume_ - 0.0f) <= std::numeric_limits<float>::epsilon()) {
+        std::abs(volume - 0.0f) <= std::numeric_limits<float>::epsilon()) {
         MEDIA_LOGI("Set volume to 0.0 for NO_RING_SOUND. Stop vibrator!");
         SystemSoundVibrator::StopVibrator();
         return MSERR_OK;
