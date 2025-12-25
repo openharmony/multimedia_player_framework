66bcd50f05acf71296ca56c381c40a92c19c1109
liuwanlin9358 <liuwanlin2@h-partners.com>
2025-12-25 22:36:34 +0800

    soundpool dfx
    Signed-off-by: liuwanlin9358 <liuwanlin2@h-partners.com>

--- frameworks/native/soundpool/soundpool.cpp
+++ frameworks/native/soundpool/soundpool.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Copyright (C) 2023 Huawei Device Co., Ltd. 2025-2025. All rights reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,12 +14,19 @@
  */
 
 #include <unistd.h>
+#include <thread>
 
 #include "media_errors.h"
 #include "media_log.h"
-#include "soundpool.h"
+#include "media_utils.h"
 #include "soundpool_manager.h"
 #include "soundpool_manager_multi.h"
+#include "soundpool.h"
+
+#ifndef CROSS_PLATFORM
+#include "nlohmann/json.hpp"
+#include "ipc_skeleton.h"
+#endif
 
 namespace {
     constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, LOG_DOMAIN_SOUNDPOOL, "SoundPool"};
@@ -27,10 +34,28 @@ namespace {
     static const int32_t SOUNDPOOL_API_VERSION_ISOLATION = 18;
     static const int32_t FAULT_API_VERSION = -1;
     static const int32_t ERROR_RETURN = -1;
+    static const char *const MODULE_NAME = "SoundPool";
+    static const char *const DFX_MSG_SUCCEED = "Succeed";
+    static const char *const DFX_MSG_FAILED = "Failed";
+    static const char *const DFX_API_LOAD = "Load";
+    static const char *const DFX_API_PLAY = "Play";
+
+    static inline void IncreaseStatistic(std::atomic<int32_t> &statisticVar)
+    {
+        statisticVar.fetch_add(1, std::memory_order_relaxed);
+    }
+
+    template <typename Task, typename ...Args> static inline void AsyncCall(Task &&task, Args ...args)
+    {
+        std::thread(std::move(task), std::forward<Args>(args)...).detach();
+    }
 }
 
 namespace OHOS {
 namespace Media {
+#ifndef CROSS_PLATFORM
+using Json = nlohmann::json;
+#endif
 std::shared_ptr<ISoundPool> SoundPoolFactory::CreateSoundPool(int maxStreams,
     const AudioStandard::AudioRendererInfo &audioRenderInfo)
 {
@@ -135,9 +160,17 @@ int32_t SoundPool::Load(const std::string &url)
 {
     MediaTrace trace("SoundPool::Load url");
     std::lock_guard lock(soundPoolLock_);
-    CHECK_AND_RETURN_RET_LOG(!url.empty(), -1, "url is empty");
-    CHECK_AND_RETURN_RET_LOG(soundIDManager_ != nullptr, -1, "soundIDManager_ has been released");
-    return soundIDManager_->Load(url);
+    
+    do {
+        CHECK_AND_BREAK_LOG(!url.empty(), "url is empty");
+        CHECK_AND_BREAK_LOG(soundIDManager_ != nullptr, "soundIDManager_ has been released");
+        const int32_t soundId = soundIDManager_->Load(url);
+        CHECK_AND_BREAK_LOG(soundId != -1, "soundIDManager load failed");
+        IncreaseStatistic(loadSucceed_);
+        return soundId;
+    } while (false);
+    IncreaseStatistic(loadFailed_);
+    return ERROR_RETURN;
 }
 
 int32_t SoundPool::Load(int32_t fd, int64_t offset, int64_t length)
@@ -146,50 +179,62 @@ int32_t SoundPool::Load(int32_t fd, int64_t offset, int64_t length)
     std::lock_guard lock(soundPoolLock_);
     MEDIA_LOGI("fd is %{public}d, offset is %{public}s, length is %{public}s", fd, std::to_string(offset).c_str(),
         std::to_string(length).c_str());
-    CHECK_AND_RETURN_RET_LOG((fd > 0 && length > 0 && offset >= 0), -1, "Invalid fd param.");
-    CHECK_AND_RETURN_RET_LOG(soundIDManager_ != nullptr, -1, "soundIDManager_ has been released");
-    return soundIDManager_->Load(fd, offset, length);
+    do {
+        CHECK_AND_BREAK_LOG((fd > 0 && length > 0 && offset >= 0), "Invalid fd param.");
+        CHECK_AND_BREAK_LOG(soundIDManager_ != nullptr, "soundIDManager_ has been released");
+        const int32_t soundId = soundIDManager_->Load(fd, offset, length);
+        CHECK_AND_BREAK_LOG(soundId != -1, "soundIDManager load failed");
+        IncreaseStatistic(loadSucceed_);
+        return soundId;
+    } while (false);
+    IncreaseStatistic(loadFailed_);
+    return ERROR_RETURN;
 }
 
 int32_t SoundPool::Play(int32_t soundID, const PlayParams &playParameters)
 {
     MediaTrace trace("SoundPool::Play");
-    MEDIA_LOGI("SoundPool::Play start");
     std::lock_guard lock(soundPoolLock_);
-    MEDIA_LOGI("SoundPool::Play, soundID is %{public}d, priority is %{public}d", soundID, playParameters.priority);
+    MEDIA_LOGI("SoundPool::Play start, soundID is %{public}d, priority is %{public}d", soundID,
+        playParameters.priority)
 
-    CHECK_AND_RETURN_RET_LOG(soundIDManager_ != nullptr, -1, "soundIDManager_ has been released");
-    std::shared_ptr<SoundParser> soundParser = soundIDManager_->GetSoundParserBySoundID(soundID);
-    CHECK_AND_RETURN_RET_LOG(soundParser != nullptr, -1, "soundParser is nullptr");
-    if (!soundParser->IsSoundParserCompleted()) {
-        MEDIA_LOGE("SoundPool::Play, soundID(%{public}d) has not been loaded completely", soundID);
-        return -1;
-    }
-    int32_t streamID = 0;
-    if (parallelStreamFlag_) {
-        CHECK_AND_RETURN_RET_LOG(parallelStreamManager_ != nullptr, ERROR_RETURN,
-            "parallelStreamManager_ has been released.");
-        streamID = parallelStreamManager_->Play(soundParser, playParameters);
+    do {
+        CHECK_AND_BREAK_LOG(soundIDManager_ != nullptr, "soundIDManager_ has been released");
+        std::shared_ptr<SoundParser> soundParser = soundIDManager_->GetSoundParserBySoundID(soundID);
+        CHECK_AND_BREAK_LOG(soundParser != nullptr, -1, "soundParser is nullptr");
+        CHECK_AND_BREAK_LOG(soundParser->IsSoundParserCompleted(),
+            "SoundPool::Play, soundID(%{public}d) has not been loaded completely", soundID);
+        int32_t streamID = ERROR_RETURN;
+        if (parallelStreamFlag_) {
+            CHECK_AND_BREAK_LOG(parallelStreamManager_ != nullptr, "parallelStreamManager_ has been released.");
+            streamID = parallelStreamManager_->Play(soundParser, playParameters);
+            MEDIA_LOGI("SounPool::Play end, streamID is %{public}d", streamID);
+            CHECK_AND_BREAK(streamID >= 0);
+            IncreaseStatistic(playSucceed_);
+            return streamID;
+        }
+
+        // New playback path
+        if (!isSetInterruptMode_) {
+            MEDIA_LOGI("interruptMode_ is %{public}d", interruptMode_);
+            streamIdManager_ = (interruptMode_ == InterruptMode::SAME_SOUND_INTERRUPT) ?
+                streamIDManagerWithSameSoundInterrupt_ : streamIDManagerWithNoInterrupt_;
+            streamIDManagerWithSameSoundInterrupt_.reset();
+            streamIDManagerWithNoInterrupt_.reset();
+            CHECK_AND_BREAK_LOG(streamIdManager_ != nullptr, "Initialize streamIdManager_ failed.");
+            int ret = streamIdManager_->InitThreadPool();
+            CHECK_AND_BREAK_LOG(ret == MSERR_OK, ERROR_RETURN, "failed to init streamIdManager");
+        }
+        isSetInterruptMode_ = true;
+        CHECK_AND_BREAK_LOG(streamIdManager_ != nullptr, "sound pool have released.");
+        streamID = streamIdManager_->Play(soundParser, playParameters);
         MEDIA_LOGI("SoundPool::Play end, streamID is %{public}d", streamID);
+        CHECK_AND_BREAK(streamID >= 0);
+        IncreaseStatistic(loadSucceed_);
         return streamID;
-    }
-
-    // New playback path
-    if (!isSetInterruptMode_) {
-        MEDIA_LOGI("interruptMode_ is %{public}d", interruptMode_);
-        streamIdManager_ = (interruptMode_ == InterruptMode::SAME_SOUND_INTERRUPT) ?
-            streamIDManagerWithSameSoundInterrupt_ : streamIDManagerWithNoInterrupt_;
-        streamIDManagerWithSameSoundInterrupt_.reset();
-        streamIDManagerWithNoInterrupt_.reset();
-        CHECK_AND_RETURN_RET_LOG(streamIdManager_ != nullptr, ERROR_RETURN, "Initialize streamIdManager_ failed.");
-        int ret = streamIdManager_->InitThreadPool();
-        CHECK_AND_RETURN_RET_LOG(ret == MSERR_OK, ERROR_RETURN, "failed to init streamIdManager");
-    }
-    isSetInterruptMode_ = true;
-    CHECK_AND_RETURN_RET_LOG(streamIdManager_ != nullptr, ERROR_RETURN, "sound pool have released.");
-    streamID = streamIdManager_->Play(soundParser, playParameters);
-    MEDIA_LOGI("SoundPool::Play end, streamID is %{public}d", streamID);
-    return streamID;
+    } while (false);
+    IncreaseStatistic(playFailed_);
+    return ERROR_RETURN;
 }
 
 int32_t SoundPool::Stop(int32_t streamID)
@@ -351,6 +396,31 @@ int32_t SoundPool::Unload(int32_t soundID)
 int32_t SoundPool::Release()
 {
     MEDIA_LOGI("SoundPool::Release");
+    
+    int32_t playSucceed = playSucceed_.exchange(0, std:memory_order_relaxed);
+    int32_t loadSucceed = loadSucceed_.exchange(0, std:memory_order_relaxed);
+    int32_t playFailed = playFailed_.exchange(0, std:memory_order_relaxed);
+    int32_t loadFailed = loadFailed_.exchange(0, std:memory_order_relaxed);
+
+    AsyncCall([playSucceed, loadSucceed, playFailed, loadFailed]() {
+        Json json;
+        json[DFX_API_LOAD][DFX_MSG_SUCCEED] = loadSucceed;
+        json[DFX_API_LOAD][DFX_MSG_FAILED] = loadFailed;
+        json[DFX_API_PLAY][DFX_MSG_SUCCEED] = playSucceed;
+        json[DFX_API_PLAY][DFX_MSG_FAILED] = playFailed;
+        std::string jsonString = json.dump();
+
+        const int32_t appUid = IPCSkeleton::GetCallingUid();
+        const std::string appName = GetClientBundleName(appUid);
+
+        HiSysEventWrite(OHOS::HiviewDFX::HiSysEvent::Domain::MULTI_MEDIA,
+            "MEIDAKIT_STATISTICS",
+            OHOS::HiviewDFX::HiSysEvent::EventType::STATISTIC,
+            "SYSCAP", MODULE_NAME,
+            "APP_NAME", appName,
+            "API_CALL", "Load|Play",
+            "MEDIA_EVENTS", jsonString);
+    });
     return ReleaseInner();
 }
 
