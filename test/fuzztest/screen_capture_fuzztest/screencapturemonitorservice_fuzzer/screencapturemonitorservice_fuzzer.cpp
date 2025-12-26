7c447d16ff2483daf813239a52608b8e15e55331
zhaoyuanbo <zhaoyuanbo3@h-partners.com>
2025-12-26 11:37:17 +0800

    设置值为随机值
    Signed-off-by: zhaoyuanbo <zhaoyuanbo3@h-partners.com>

--- test/fuzztest/screen_capture_fuzztest/screencapturemonitorservice_fuzzer/screencapturemonitorservice_fuzzer.cpp
+++ test/fuzztest/screen_capture_fuzztest/screencapturemonitorservice_fuzzer/screencapturemonitorservice_fuzzer.cpp
@@ -26,6 +26,28 @@ using namespace Media;
 namespace OHOS {
 namespace Media {
 
+namespace {
+    const uint8_t* g_data = nullptr;
+    size_t g_size = 0;
+    size_t g_pos;
+}
+
+template<class T>
+T GetData()
+{
+    T object {};
+    size_t objectSize = sizeof(object);
+    if (g_data == nullptr || objectSize > g_size - g_pos) {
+        return object;
+    }
+    errno_t ret = memcpy_s(&object, objectSize, g_data + g_pos, objectSize);
+    if (ret != EOK) {
+        return {};
+    }
+    g_pos += objectSize;
+    return object;
+}
+
 ScreenCaptureMonitorListenerTest::ScreenCaptureMonitorListenerTest()
 {
 }
@@ -59,6 +81,10 @@ bool ScreenCaptureMonitorServiceFuzzer::FuzzScreenCaptureMonitorCase(uint8_t *da
     if (data == nullptr || size < sizeof(int64_t)) {
         return true;
     }
+    
+    g_data = data;
+    g_size = size;
+    g_pos = 0;
 
     std::shared_ptr<ScreenCaptureMonitorServer> screenCaptureMonitorServer =
         ScreenCaptureMonitorServer::GetInstance();
