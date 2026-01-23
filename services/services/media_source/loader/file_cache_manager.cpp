453055137f7eeeee901099362c7450f7ba2e4963
gcw_4z3jiPvB <huweinan1@h-partners.com>
2026-01-23 17:20:52 +0800

    update: 更新文件 file_cache_manager.cpp
    Signed-off-by: gcw_4z3jiPvB <huweinan1@h-partners.com>

--- services/services/media_source/loader/file_cache_manager.cpp
+++ services/services/media_source/loader/file_cache_manager.cpp
@@ -112,7 +112,7 @@ bool FileCacheManager::IsValid(const std::string& path, int64_t exceptedSize)
     int64_t actual = GetSize(path);
     if (actual != exceptedSize) {
         Clear(path);
-        MEDIA_LOG_D("Segment is invalid: excepted %ld, got %ld", exceptedSize, actual);
+        MEDIA_LOG_D("Segment is invalid: excepted %lld, got %lld", exceptedSize, actual);
         return false;
     }
     return true;
