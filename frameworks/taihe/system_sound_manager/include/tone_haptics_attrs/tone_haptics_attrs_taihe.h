9c9f574818233a2da25d5f1ba0cd219fd95c9e88
imyuhang <liyuhang24@huawei.com>
2026-01-29 19:56:12 +0800

    update: 更新文件 tone_haptics_attrs_taihe.h
    Signed-off-by: imyuhang <liyuhang24@huawei.com>

--- frameworks/taihe/system_sound_manager/include/tone_haptics_attrs/tone_haptics_attrs_taihe.h
+++ frameworks/taihe/system_sound_manager/include/tone_haptics_attrs/tone_haptics_attrs_taihe.h
@@ -24,7 +24,7 @@
 #include "tone_haptics_attrs.h"
 
 namespace ANI::Media {
-using namespace ohos::multimedia::systemsoundmanager;
+using namespace ohos::multimedia::systemSoundmanager;
 static const std::string TONE_HAPTICS_ATTRS_TAIHE_CLASS_NAME = "ToneHapticsAttrs";
 
 class ToneHapticsAttrsImpl {
