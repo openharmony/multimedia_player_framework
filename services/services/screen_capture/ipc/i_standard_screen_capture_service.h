3626f927a81e1ab0ca3134dbcfe7710a3e3651a2
zhaoyuanbo <zhaoyuanbo3@h-partners.com>
2026-01-31 18:11:50 +0800

    修改AI告警
    Signed-off-by: zhaoyuanbo <zhaoyuanbo3@h-partners.com>

--- services/services/screen_capture/ipc/i_standard_screen_capture_service.h
+++ services/services/screen_capture/ipc/i_standard_screen_capture_service.h
@@ -50,7 +50,7 @@ public:
     virtual int32_t SetCanvasRotation(bool canvasRotation) = 0;
     virtual int32_t ShowCursor(bool showCursor) = 0;
     virtual int32_t ResizeCanvas(int32_t width, int32_t height) = 0;
-    virtual int32_t SkipPrivacyMode(std::vector<uint64_t> &windowIDsVec) = 0;
+    virtual int32_t SkipPrivacyMode(const std::vector<uint64_t> &windowIDsVec) = 0;
     virtual int32_t SetMaxVideoFrameRate(int32_t frameRate) = 0;
     virtual int32_t SetListenerObject(const sptr<IRemoteObject> &object) = 0;
     virtual int32_t AcquireAudioBuffer(std::shared_ptr<AudioBuffer> &audioBuffer, AudioCaptureSourceType type) = 0;
