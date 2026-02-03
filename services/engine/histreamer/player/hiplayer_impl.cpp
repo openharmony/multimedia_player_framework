b8f490e66dc8273cfe680cedae6a7c1d1bd5f7bf
孙志威 <sunzhiwei13@h-partners.com>
2026-02-03 14:30:45 +0800

    修改安全告警中检视意见
    Signed-off-by: 孙志威 <sunzhiwei13@h-partners.com>

--- services/engine/histreamer/player/hiplayer_impl.cpp
+++ services/engine/histreamer/player/hiplayer_impl.cpp
@@ -3184,8 +3184,7 @@ void HiPlayerImpl::HandleCompleteEvent(const Event& event)
     }
     callbackLooper_.DoReportCompletedTime();
     FALSE_RETURN_MSG(pipeline_ != nullptr, "pipeline_ is nullptr");
-    if (pipelineStates_ != PlayerStates::PLAYER_FROZEN ||
-        (pipelineStates_ == PlayerStates::PLAYER_FROZEN && !singleLoop_.load())) {
+    if (pipelineStates_ != PlayerStates::PLAYER_FROZEN || !singleLoop_.load()) {
         pipeline_->Pause();
     }
     if (isSetPlayRange_ && (startTimeWithMode_ == PLAY_RANGE_DEFAULT_VALUE ||
