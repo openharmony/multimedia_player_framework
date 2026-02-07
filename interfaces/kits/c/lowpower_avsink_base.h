2acaa4f64c8ca1f52a69fd8aaae8bdecb4678e68
zhupengfei <zhupengfei26@huawei.com>
2026-02-07 11:43:06 +0800

    update: 更新文件 lowpower_avsink_base.h
    Signed-off-by: zhupengfei <zhupengfei26@huawei.com>

--- interfaces/kits/c/lowpower_avsink_base.h
+++ interfaces/kits/c/lowpower_avsink_base.h
@@ -1,56 +1,61 @@
-/*
- * Copyright (c) 2025 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License"),
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
- 
-/**
- * @addtogroup AVSinkBase
- * @{
- *
- * @brief The AVSinkBase module provides variables, properties, and functions
- * for lowpower audio sink and lowpower video sink.
- *
- * @since 20
- */
- 
-/**
- * @file lowpower_avsink_base.h
- *
- * @brief Declare the Native API used for lowpower audio sink
- * and lowpower video sink.
- *
- * @library liblowpower_avsink.so
- * @kit MediaKit
- * @syscap SystemCapability.Multimedia.Media.LowPowerAVSink
- * @since 20
- */
- 
-#ifndef NATIVE_LOWPOWER_AVSINK_BASE_H
-#define NATIVE_LOWPOWER_AVSINK_BASE_H
- 
-#include <stdint.h>
-#include "native_averrors.h"
-#include "native_avbuffer.h"
- 
-#ifdef __cplusplus
-extern "C" {
-#endif
- 
-/**
- * @brief Forward declaration of OH_AVSamplesBuffer.
- *
- * @since 20
- */
+OpenHarmony
+同步事件侵入式消分备案黄蓝差异黄蓝差异(CR)个人提交跟踪看板项目提交跟踪看板DTS问题单看板
+Welcome, z00654174 
+黄蓝差异 详情
+multimedia_player_framework黄蓝差异详情
+test/unittest/lpp_unit_test/module/lpp_sync_mgr_adapter_unit_test/mock/v1_0/ilow_power_player_factory.h
+multimedia_player_framework
+master
+huawei/EMUI/HarmonyOS/hmos_trunk_dev_20260115/OpenHarmony-Trunk
+待处理
+--
+--
+test/fuzztest/lowpoweravsink_fuzztest/lowpowervideosink_fuzzer/lowpowervideosink_fuzzer.cpp
+multimedia_player_framework
+master
+huawei/EMUI/HarmonyOS/hmos_trunk_dev_20260115/OpenHarmony-Trunk
+待处理
+--
+--
+test/fuzztest/lowpoweravsink_fuzztest/lowpowervideosink_fuzzer/BUILD.gn
+multimedia_player_framework
+master
+huawei/EMUI/HarmonyOS/hmos_trunk_dev_20260115/OpenHarmony-Trunk
+待处理
+--
+--
+test/fuzztest/lowpoweravsink_fuzztest/lowpoweraudiosink_fuzzer/lowpoweraudiosink_fuzzer.cpp
+multimedia_player_framework
+master
+huawei/EMUI/HarmonyOS/hmos_trunk_dev_20260115/OpenHarmony-Trunk
+待处理
+--
+--
+test/fuzztest/lowpoweravsink_fuzztest/lowpoweraudiosink_fuzzer/BUILD.gn
+multimedia_player_framework
+master
+huawei/EMUI/HarmonyOS/hmos_trunk_dev_20260115/OpenHarmony-Trunk
+待处理
+--
+--
+interfaces/kits/c/lowpower_avsink_base.h
+multimedia_player_framework
+master
+huawei/EMUI/HarmonyOS/hmos_trunk_dev_20260115/OpenHarmony-Trunk
+待处理
+--
+--
+共 6 条
+10条/页
+
+
+前往
+1
+页
+interfaces/kits/c/lowpower_avsink_base.h-黄蓝区差异详情
+
+蓝区代码
+黄区代码
 typedef struct OH_AVSamplesBuffer OH_AVSamplesBuffer;
 
 /**
@@ -62,30 +67,6 @@ typedef struct OH_LowPowerAVSink_Capability OH_LowPowerAVSink_Capability;
 
 /**
  * @brief Append one OH_AVBuffer data to framePacketBuffer instance.
- *
- * @param samplesBuffer OH_AVSamplesBuffer instance
- * @param avBuffer OH_AVBuffer buffer will be appended to
- * @return Returns AV_ERR_OK if the execution is successful,
- * otherwise returns a specific error code, refer to {@link OH_AVErrCode}.
- * {@link AV_ERR_INVALID_VAL}, the samplesBuffer or the avBuffer or data pointer is nullptr or invalid.
- * {@link AV_ERR_NO_MEMORY}, the framePacketBuffer has no enough remained capacity to append one OH_AVBuffer.
- * {@link AV_ERR_UNKNOWN}, unknown error.
- * @since 20
- */
-OH_AVErrCode OH_AVSamplesBuffer_AppendOneBuffer(OH_AVSamplesBuffer *samplesBuffer, OH_AVBuffer *avBuffer);
- 
-/**
- * @brief Get remaining capacity of OH_AVSamplesBuffer instance.
- *
- * @param {OH_AVSamplesBuffer} samplesBuffer OH_AVSamplesBuffer instance
- * @return Returns remained capacity of OH_AVSamplesBuffer instance,
- * return -1 if samplesBuffer or data poniter is is nullptr or invalid.
- * @since 20
- */
-int32_t OH_AVSamplesBuffer_GetRemainedCapacity(OH_AVSamplesBuffer *samplesBuffer);
-
-/**
- * @brief Query the supported capabilities of a lowpower audio/video sink.
  *
  * This function queries and returns the capability set supported by the current
  * lowpower audio/video sink, including but not limited to supported media formats, etc.
@@ -99,7 +80,27 @@ int32_t OH_AVSamplesBuffer_GetRemainedCapacity(OH_AVSamplesBuffer *samplesBuffer
 OH_LowPowerAVSink_Capability *OH_LowPowerAVSink_GetCapability();
 #ifdef __cplusplus
 }
-#endif
-#endif // NATIVE_LOWPOWER_AVSINK_BASE_H
- 
-/** @} */
+typedef struct OH_AVSamplesBuffer OH_AVSamplesBuffer;
+
+/**
+ * @brief Forward declaration of OH_LowPowerAVSink_Capability.
+ *
+ * @since 20
+ */
+typedef struct OH_LowPowerAVSink_Capability OH_LowPowerAVSink_Capability;
+
+/**
+ * @brief Append one OH_AVBuffer data to framePacketBuffer instance.
+ *
+ * This function queries and returns the capability set supported by the current
+ * lowpower audio/video sink, including but not limited to supported media formats, etc.
+ *
+ * @return {OH_LowPowerAVSink_Capability*}
+ * - A pointer to the capability structure if the sink supports capability queries and the query is successful.
+ * - nullptr if the sink does not support capability queries or the query fails.
+
+ * @since 21
+ */
+OH_LowPowerAVSink_Capability *OH_LowPowerAVSink_GetCapability();
+#ifdef __cplusplus
+}
