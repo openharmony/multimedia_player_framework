aab7227daf06f6f597a3fb66d010686a0c6c489c
zhupengfei <zhupengfei26@huawei.com>
2026-02-26 14:32:28 +0800

    update: 更新文件 lpp_video_streamer_client.cpp
    Signed-off-by: zhupengfei <zhupengfei26@huawei.com>

--- services/services/lpp_video_streamer/client/lpp_video_streamer_client.cpp
+++ services/services/lpp_video_streamer/client/lpp_video_streamer_client.cpp
@@ -58,7 +58,7 @@ int32_t LppVideoStreamerClient::SetParameter(const Format &param)
     CHECK_AND_RETURN_RET_LOG(playerProxy_ != nullptr, MSERR_SERVICE_DIED, "player service does not exist..");
     return playerProxy_->SetParameter(param);
 }
- 
+
 int32_t LppVideoStreamerClient::GetParameter(Format &param)
 {
     std::lock_guard<std::mutex> lock(mutex_);
