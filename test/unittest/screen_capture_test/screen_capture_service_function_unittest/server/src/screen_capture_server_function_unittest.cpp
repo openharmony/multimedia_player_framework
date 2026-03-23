3e64b12db828bdaa007c8b7084366d804290db00
zeng <zengxi14@h-partners.com>
2026-03-24 07:16:38 +0800

    keyboard privacy eight
    Signed-off-by: zeng <zengxi14@h-partners.com>

--- test/unittest/screen_capture_test/screen_capture_service_function_unittest/server/src/screen_capture_server_function_unittest.cpp
+++ test/unittest/screen_capture_test/screen_capture_service_function_unittest/server/src/screen_capture_server_function_unittest.cpp
@@ -2845,35 +2845,41 @@ HWTEST_F(ScreenCaptureServerFunctionTest, GetValueFromJson_004, TestSize.Level2)
     EXPECT_EQ(value, true);
 }
 
-HWTEST_F(ScreenCaptureServerFunctionTest, SystemPrivacyProtected_001, TestSize.Level2)
+HWTEST_F(ScreenCaptureServerFunctionTest, PrivacyProtected_001, TestSize.Level2)
 {
     ScreenId virtualScreenId = 1;
     bool systemPrivacyProtectionSwitch = true;
-    screenCaptureServer_->SystemPrivacyProtected(virtualScreenId, systemPrivacyProtectionSwitch);
+    bool appPrivacyProtectionSwitch = true;
+    screenCaptureServer_->PrivacyProtected(virtualScreenId, systemPrivacyProtectionSwitch,
+        appPrivacyProtectionSwitch);
     EXPECT_EQ(screenCaptureServer_->captureState_, AVScreenCaptureState::CREATED);
 }
 
-HWTEST_F(ScreenCaptureServerFunctionTest, SystemPrivacyProtected_002, TestSize.Level2)
+HWTEST_F(ScreenCaptureServerFunctionTest, PrivacyProtected_002, TestSize.Level2)
 {
     SetValidConfig();
     ASSERT_EQ(InitStreamScreenCaptureServer(), MSERR_OK);
     ASSERT_EQ(screenCaptureServer_->StartScreenCapture(false), MSERR_OK);
     ScreenId virtualScreenId = screenCaptureServer_->virtualScreenId_;
     bool systemPrivacyProtectionSwitch = true;
-    screenCaptureServer_->SystemPrivacyProtected(virtualScreenId, systemPrivacyProtectionSwitch);
+    bool appPrivacyProtectionSwitch = true;
+    screenCaptureServer_->PrivacyProtected(virtualScreenId, systemPrivacyProtectionSwitch,
+        appPrivacyProtectionSwitch);
     EXPECT_EQ(screenCaptureServer_->captureState_, AVScreenCaptureState::STARTED);
     screenCaptureServer_->StopScreenCapture();
     screenCaptureServer_->Release();
 }
 
-HWTEST_F(ScreenCaptureServerFunctionTest, SystemPrivacyProtected_003, TestSize.Level2)
+HWTEST_F(ScreenCaptureServerFunctionTest, PrivacyProtected_003, TestSize.Level2)
 {
     SetValidConfig();
     ASSERT_EQ(InitStreamScreenCaptureServer(), MSERR_OK);
     ASSERT_EQ(screenCaptureServer_->StartScreenCapture(false), MSERR_OK);
     ScreenId virtualScreenId = screenCaptureServer_->virtualScreenId_;
     bool systemPrivacyProtectionSwitch = false;
-    screenCaptureServer_->SystemPrivacyProtected(virtualScreenId, systemPrivacyProtectionSwitch);
+    bool appPrivacyProtectionSwitch = false;
+    screenCaptureServer_->PrivacyProtected(virtualScreenId, systemPrivacyProtectionSwitch,
+        appPrivacyProtectionSwitch);
     EXPECT_EQ(screenCaptureServer_->captureState_, AVScreenCaptureState::STARTED);
     screenCaptureServer_->StopScreenCapture();
     screenCaptureServer_->Release();
