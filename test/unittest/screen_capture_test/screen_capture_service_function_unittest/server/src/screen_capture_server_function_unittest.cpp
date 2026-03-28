ca8f146f651b9723b830c7708c40955b06f13b79
2301_76695984 <zengxi14@h-partners.com>
2026-03-28 20:06:58 +0800

    提高7
    Signed-off-by: 2301_76695984 <zengxi14@h-partners.com>

--- test/unittest/screen_capture_test/screen_capture_service_function_unittest/server/src/screen_capture_server_function_unittest.cpp
+++ test/unittest/screen_capture_test/screen_capture_service_function_unittest/server/src/screen_capture_server_function_unittest.cpp
@@ -3446,6 +3446,24 @@ HWTEST_F(ScreenCaptureServerFunctionTest, HandleOriginalStreamPrivacy_003, TestS
     EXPECT_EQ(screenCaptureServer_->checkBoxSelected_, false);
 }
 
+HWTEST_F(ScreenCaptureServerFunctionTest, HandleOriginalStreamPrivacy_006, TestSize.Level2)
+{
+    screenCaptureServer_->captureConfig_.dataType = DataType::INVAILD;
+    screenCaptureServer_->checkBoxSelected_ = false;
+    EXPECT_EQ(screenCaptureServer_->HandleOriginalStreamPrivacy(), MSERR_OK);
+}
+
+HWTEST_F(ScreenCaptureServerFunctionTest, HandleOriginalStreamPrivacy_007, TestSize.Level2)
+{
+    SetValidConfig();
+    ASSERT_EQ(InitStreamScreenCaptureServer(), MSERR_OK);
+    ASSERT_EQ(screenCaptureServer_->StartScreenCapture(false), MSERR_OK);
+    screenCaptureServer_->checkBoxSelected_ = false;
+    EXPECT_EQ(screenCaptureServer_->HandleOriginalStreamPrivacy(), MSERR_OK);
+    screenCaptureServer_->StopScreenCapture();
+    screenCaptureServer_->Release();
+}
+
 HWTEST_F(ScreenCaptureServerFunctionTest, RegisterLanguageSwitchListener_001, TestSize.Level2)
 {
     screenCaptureServer_->RegisterLanguageSwitchListener();
