f53d687b9c64e44a639b905b4da6479a28e9f53c
zhaoyuanbo <zhaoyuanbo3@h-partners.com>
2026-04-17 16:31:41 +0800

    修改规范
    Signed-off-by: zhaoyuanbo <zhaoyuanbo3@h-partners.com>

--- interfaces/kits/c/native_avscreen_capture_base.h
+++ interfaces/kits/c/native_avscreen_capture_base.h
@@ -623,13 +623,13 @@ typedef enum OH_CapturePickerMode {
      * @brief Show both screen and application options
      * @since 26.0.0
      */
- 	OH_CAPTURE_PICKER_MODE_SCREEN_AND_APP = 5,
+    OH_CAPTURE_PICKER_MODE_SCREEN_AND_APP = 5,
  	
     /**
      * @brief Show screen, window, and application options
      * @since 26.0.0
      */
- 	OH_CAPTURE_PICKER_MODE_SCREEN_WINDOW_AND_APP = 6,
+    OH_CAPTURE_PICKER_MODE_SCREEN_WINDOW_AND_APP = 6,
 } OH_CapturePickerMode;
 
 /**
