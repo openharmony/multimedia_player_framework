c1eec2970a005142be19c871be8e19f85b8a28c0
SunYin <sunyin5@huawei.com>
2026-07-20 15:02:22 +0800

    Optimize AVMeta Co-authored-by:Agent
    Signed-off-by: SunYin <sunyin5@huawei.com>

--- frameworks/native/avmetadatahelper/avmetadatahelper_impl.cpp
+++ frameworks/native/avmetadatahelper/avmetadatahelper_impl.cpp
@@ -653,7 +653,7 @@ int32_t AVMetadataHelperImpl::CopySurfaceBufferToPixelMap(sptr<SurfaceBuffer> &s
     
     int64_t uvOffset = static_cast<int64_t>(stride) * sliceHeight;
     CHECK_AND_RETURN_RET_LOG(uvOffset > 0 && uvOffset < static_cast<int64_t>(surfaceBuffer->GetSize()),
-        MSERR_INVALID_VAL, "uvOffset(%{public}lld) is invalid", uvOffset);
+        MSERR_INVALID_VAL, "uvOffset(%{public}ld) is invalid", uvOffset);
     srcPtr = static_cast<uint8_t *>(surfaceBuffer->GetVirAddr()) + uvOffset;
     
     // copy src UV plane to dst, height(UV) = height(Y) / 2
