d64ec6b35bb1e7d1c71946247a22d2db26b80cf4
zhaoyuanbo <zhaoyuanbo3@h-partners.com>
2026-09-04 11:05:33 +0800

    删除空格
    Signed-off-by: zhaoyuanbo <zhaoyuanbo3@h-partners.com>

--- test/fuzztest/screen_capture_fuzztest/screencaptureaudiobitratefile_fuzzer/screencaptureaudiobitratefile_fuzzer.cpp
+++ test/fuzztest/screen_capture_fuzztest/screencaptureaudiobitratefile_fuzzer/screencaptureaudiobitratefile_fuzzer.cpp
@@ -87,8 +87,6 @@ void SetConfig(AVScreenCaptureConfig &config)
     };
 }
 
-
-
 bool ScreenCaptureAudioBitRateFileFuzzer::FuzzScreenCaptureAudioBitRateFile(uint8_t *data, size_t size)
 {
     if (data == nullptr || size < sizeof(int32_t)) {
